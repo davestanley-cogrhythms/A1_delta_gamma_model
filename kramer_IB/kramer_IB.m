ce18b19ed7e63983170abdbd77d6a67e3eb9e2f4
David Stanley <davearthur.stanley@gmail.com>
2016-04-17 17:25:15 -0400

    5_single_comp_Ca_All currents, fixed calcium.

--- kramer_IB/kramer_IB.m
+++ kramer_IB/kramer_IB.m
@@ -10,7 +10,7 @@ N=2;
 
 % tonic input currents
 Jd=23.5; % apical: 23.5(25.5), basal: 23.5(42.5)
-Js=-6; % -4.5
+Js=-8.5; % -4.5
 Ja=-6;   % -6(-.4)
 
 % Poisson IPSPs to IBdb (basal dendrite)
@@ -18,7 +18,7 @@ gRAN=125;
 
 % some intrinsic currents
 gAR_L=50;  % 50,  LTS - max conductance of h-channel
-gAR_d=0; % 155, IBda - max conductance of h-channel
+gAR_d=155; % 155, IBda - max conductance of h-channel
 
 % connection strengths
 gad=0;      % IBa -> IBdb, 0(.04)
@@ -30,7 +30,7 @@ ggja=.002;  % IBa -> IBa
 
 % Compartmental parameters
 gM_s = 1.5;         % Value from axon
-gCaH_s = 0;
+gCaH_s = 6.5;
 
 
 % constant biophysical parameters
@@ -49,10 +49,10 @@ spec.nodes(i).multiplicity = N;
 spec.nodes(i).dynamics = {'V''=(current)/Cm'};
 spec.nodes(i).mechanisms = {'IBs_itonic','IBs_noise','IBs_iNaF','IBs_iKDR','IBda_iAR','IBda_iM','IBda_iCaH','IBs_leak'};
 spec.nodes(i).parameters = {...
-  'V_IC',-65,'IC_noise',IC_noise,'Cm',Cm,'E_l',-70,'g_l',0.25,...
+  'V_IC',-65,'IC_noise',IC_noise,'Cm',Cm,'E_l',-70,'g_l',1,...
   'stim',Js,'onset',0,'V_noise',0,...
-  'gNaF',100,'E_NaF',ENa,'NaF_V0',34.5,'NaF_V1',59.4,'NaF_d1',10.7,'NaF_V2',33.5,'NaF_d2',15,'NaF_c0',.15,'NaF_c1',1.15,...
-  'gKDR',5,'E_KDR',E_EKDR,'KDR_V1',29.5,'KDR_d1',10,'KDR_V2',10,'KDR_d2',10,...
+  'gNaF',50,'E_NaF',ENa,'NaF_V0',34.5,'NaF_V1',59.4,'NaF_d1',10.7,'NaF_V2',33.5,'NaF_d2',15,'NaF_c0',.15,'NaF_c1',1.15,...
+  'gKDR',10,'E_KDR',E_EKDR,'KDR_V1',29.5,'KDR_d1',10,'KDR_V2',10,'KDR_d2',10,...
   'gAR',gAR_d,'E_AR',IB_Eh,'AR_V12',-87.5,'AR_k',-5.5,'c_ARaM',2.75,'c_ARbM',3,'AR_L',1,'AR_R',1,...
   'gM',gM_s,'E_M',E_EKDR,'c_MaM',1,'c_MbM',1,...
   'gCaH',gCaH_s,'E_CaH',ECa,'tauCaH',.33333,'c_CaHaM',3,'c_CaHbM',3,...
