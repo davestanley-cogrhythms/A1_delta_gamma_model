3c0997645e2d0663c797cf52bc66a53e3a83c96c
David Stanley <davearthur.stanley@gmail.com>
2016-05-11 17:15:21 -0400

    Added option for plotting with all noise removed.

--- kramer_IB_dnsim/kramer_IB.m
+++ kramer_IB_dnsim/kramer_IB.m
@@ -1,8 +1,11 @@
 %% Model: Kramer 2008, PLoS Comp Bio
 
 % simulation controls
-tspan=[0 100]; dt=.01; solver='euler'; % euler, rk2, rk4
+tspan=[0 200]; dt=.01; solver='euler'; % euler, rk2, rk4
 dsfact=1; % downsample factor, applied after simulation
+
+% No noise simulation
+no_noise = 0;
 
 % number of cells per population
 N=2;
@@ -27,6 +30,12 @@ gas=.3;     % IBa -> IBs
 gsa=.3;     % IBs -> IBa
 ggja=.002;  % IBa -> IBa
 
+% Current injection noise levels
+IBda_Vnoise = .1;
+IBs_Vnoise = 0;
+IBdb_Vnoise = .1;
+IBa_Vnoise = 5.5;
+
 % constant biophysical parameters
 Cm=.9;        % membrane capacitance
 ENa=50;      % sodium reversal potential
@@ -34,6 +43,13 @@ E_EKDR=-95;  % potassium reversal potential for excitatory cells
 IB_Eh=-25;   % h-current reversal potential for deep layer IB cells
 ECa=125;     % calcium reversal potential
 IC_noise=.01;% fractional noise in initial conditions
+if no_noise
+    IC_noise= 0;
+    IBda_Vnoise = 0;
+    IBs_Vnoise = 0;
+    IBdb_Vnoise = 0;
+    IBa_Vnoise = 0;
+end
 
 V_ICvar = -0;
 
@@ -44,7 +60,7 @@ spec.nodes(1).dynamics = {'V''=(current)/Cm'};
 spec.nodes(1).mechanisms = {'IBdaitonic','IBdanoise','IBdaiNaF','IBdaiKDR','IBdaiAR','IBdaiM','IBdaiCaH','IBdaleak'};
 spec.nodes(1).parameters = {...
   'V_IC',V_ICvar,'IC_noise',IC_noise,'Cm',Cm,'E_l',-70,'g_l',2,...
-  'stim',Jd,'onset',0,'V_noise',.1,...
+  'stim',Jd,'onset',0,'V_noise',IBda_Vnoise,...
   'gNaF',125,'E_NaF',ENa,'NaF_V0',34.5,'NaF_V1',59.4,'NaF_d1',10.7,'NaF_V2',33.5,'NaF_d2',15,'NaF_c0',.15,'NaF_c1',1.15,...
   'gKDR',10,'E_KDR',E_EKDR,'KDR_V1',29.5,'KDR_d1',10,'KDR_V2',10,'KDR_d2',10,...
   'gAR',gAR_d,'E_AR',IB_Eh,'AR_V12',-87.5,'AR_k',-5.5,'c_ARaM',2.75,'c_ARbM',3,'AR_L',1,'AR_R',1,...
@@ -58,7 +74,7 @@ spec.nodes(i).dynamics = {'V''=(current)/Cm'};
 spec.nodes(i).mechanisms = {'IBsitonic','IBsnoise','IBsiNaF','IBsiKDR','IBsleak'};
 spec.nodes(i).parameters = {...
   'V_IC',V_ICvar,'IC_noise',IC_noise,'Cm',Cm,'E_l',-70,'g_l',1,...
-  'stim',Js,'onset',0,'V_noise',0,...
+  'stim',Js,'onset',0,'V_noise',IBs_Vnoise,...
   'gNaF',50,'E_NaF',ENa,'NaF_V0',34.5,'NaF_V1',59.4,'NaF_d1',10.7,'NaF_V2',33.5,'NaF_d2',15,'NaF_c0',.15,'NaF_c1',1.15,...
   'gKDR',10,'E_KDR',E_EKDR,'KDR_V1',29.5,'KDR_d1',10,'KDR_V2',10,'KDR_d2',10,...
   };
@@ -68,7 +84,7 @@ spec.nodes(3).dynamics = {'V''=(current)/Cm'};
 spec.nodes(3).mechanisms = {'IBdbitonic','IBdbnoise','IBdbiNaF','IBdbiKDR','IBdbiAR','IBdbiM','IBdbiCaH','IBdbleak'};
 spec.nodes(3).parameters = {... % same as IBda except gAR=115, + IPSP params
   'V_IC',V_ICvar,'IC_noise',IC_noise,'Cm',Cm,'E_l',-70,'g_l',2,...
-  'stim',Jd,'onset',0,'V_noise',.1,'gRAN',gRAN,'ERAN',-80,'tauRAN',4,...
+  'stim',Jd,'onset',0,'V_noise',IBdb_Vnoise,'gRAN',gRAN,'ERAN',-80,'tauRAN',4,...
   'gNaF',125,'E_NaF',ENa,'NaF_V0',34.5,'NaF_V1',59.4,'NaF_d1',10.7,'NaF_V2',33.5,'NaF_d2',15,'NaF_c0',.15,'NaF_c1',1.15,...
   'gKDR',10,'E_KDR',E_EKDR,'KDR_V1',29.5,'KDR_d1',10,'KDR_V2',10,'KDR_d2',10,...
   'gAR',115,'E_AR',IB_Eh,'AR_V12',-87.5,'AR_k',-5.5,'c_ARaM',2.75,'c_ARbM',3,'AR_L',1,'AR_R',1,...
@@ -83,7 +99,7 @@ spec.nodes(i).dynamics = {'V''=(current)/Cm'};
 spec.nodes(i).mechanisms = {'IBaitonic','IBanoise','IBaiNaF','IBaiKDR','IBaiM','IBaleak'};
 spec.nodes(i).parameters = {...
   'V_IC',V_ICvar,'IC_noise',IC_noise,'Cm',Cm,'E_l',-70,'g_l',.25,...
-  'stim',Ja,'onset',0,'V_noise',5.5,...
+  'stim',Ja,'onset',0,'V_noise',IBa_Vnoise,...
   'gNaF',100,'E_NaF',ENa,'NaF_V0',34.5,'NaF_V1',59.4,'NaF_d1',10.7,'NaF_V2',33.5,'NaF_d2',15,'NaF_c0',.15,'NaF_c1',1.15,...
   'gKDR',5,'E_KDR',E_EKDR,'KDR_V1',29.5,'KDR_d1',10,'KDR_V2',10,'KDR_d2',10,...
   'gM',1.5,'E_M',E_EKDR,'c_MaM',1.5,'c_MbM',.75,...
@@ -112,21 +128,6 @@ spec.connections(4,3).parameters = {'g_SYN',gad,'E_SYN',0,'tauDx',100,'tauRx',.5
 spec.connections(4,4).label = 'IBa-IBa';
 spec.connections(4,4).mechanisms = {'IBaIBaiGAP'};
 spec.connections(4,4).parameters = {'g_GAP',ggja,'fanout',inf};
-
-
-% ggja = .2;
-% spec.connections(2,2).label = 'IBa-IBa';
-% spec.connections(2,2).mechanisms = {'IBaIBaiGAP'};
-% spec.connections(2,2).parameters = {'g_GAP',ggja,'fanout',inf};
-% 
-% 
-% spec.connections(1,2).label = 'IBs-IBa';
-% spec.connections(1,2).mechanisms = {'IBsIBaiCOM'};
-% spec.connections(1,2).parameters = {'g_COM',gsa,'comspan',.5};
-% 
-% spec.connections(2,1).label = 'IBa-IBs';
-% spec.connections(2,1).mechanisms = {'IBaIBsiCOM'};
-% spec.connections(2,1).parameters = {'g_COM',gas,'comspan',.5};
 
 
 % process specification and simulate model
