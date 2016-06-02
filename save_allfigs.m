d42dfd30d83a0f3a21a4d8d3106871ce1690174b
David Stanley <davearthur.stanley@gmail.com>
2016-06-02 16:41:31 -0400

    10e-GABAB_play_Restore_10c_9d Restored network configuration of 10c and 9d..

--- save_allfigs.m
+++ save_allfigs.m
@@ -6,7 +6,7 @@ function save_allfigs(currfname,currfigname)
 %     if ~exist('currfname'); currfname = 'kramer_IB'; end
 %     if ~exist('currfigname'); currfigname = '3_single_comp_only_Mcurr'; end
     currfname = 'kramer_IB'; 
-    currfigname = '10d-GABAB_play_Tmax2';
+    currfigname = '10e-GABAB_play_Restore_10c_9d';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -15,7 +15,7 @@ function save_allfigs(currfname,currfigname)
     basepath = '.';
     % basepath = '~/figs_tosave';
     mkdir(fullfile(basepath,sp));
-    for i=[1:3]
+    for i=[1:1]
         figure(i); %ylim([0 0.175])
         %title('');
         %ylabel('');
@@ -28,7 +28,7 @@ function save_allfigs(currfname,currfigname)
         %close
     end
     
-    mycomment = ['This is a network with very strong NG->IB GABAB conductance, but wearker activation of GABAB by only a few NG APs.'];
+    mycomment = ['Restored network configuration of 10c and 9d..'];
     currd = pwd;
     cd ..
     system('git add *');
