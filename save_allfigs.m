259838140618f7cbfacdc76e6d352d09424e0971
David Stanley <davearthur.stanley@gmail.com>
2016-06-04 22:30:20 -0400

    17b_NG_gA_40 iApp sweep. gA=40. No NG->NG GABA connectivity.

--- save_allfigs.m
+++ save_allfigs.m
@@ -6,7 +6,7 @@ function save_allfigs(currfname,currfigname)
 %     if ~exist('currfname'); currfname = 'kramer_IB'; end
 %     if ~exist('currfigname'); currfigname = '3_single_comp_only_Mcurr'; end
     currfname = 'kramer_IB'; 
-    currfigname = '17a_NG_gA_60';
+    currfigname = '17b_NG_gA_40';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -28,7 +28,7 @@ function save_allfigs(currfname,currfigname)
         %close
     end
     
-    mycomment = ['iApp sweep. gA=60. No NG->NG GABA connectivity.'];
+    mycomment = ['iApp sweep. gA=40. No NG->NG GABA connectivity.'];
     currd = pwd;
     cd ..
     system('git add *');
