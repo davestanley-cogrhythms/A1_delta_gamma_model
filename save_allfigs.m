110849ba2a4769a81428fd696b094c9eb5f787e2
David Stanley <davearthur.stanley@gmail.com>
2016-06-07 01:13:44 -0400

    22a-NG_iA_delta: Working in delta network (NG + IB cells). Increased NG gA to 60 in order to test out delayed spiking. Figs: 1 - no NMDA 2,3 - NMDA 5 cells 4,5,6 - NMDA 10 cells Reduced iCaH and iM from (2,2) to (1,1). 7 - No NMDA 8 - gNMDA = 5 9 - gNMDA = 10

--- save_allfigs.m
+++ save_allfigs.m
@@ -6,7 +6,7 @@ function save_allfigs(currfname,currfigname)
 %     if ~exist('currfname'); currfname = 'kramer_IB'; end
 %     if ~exist('currfigname'); currfigname = '3_single_comp_only_Mcurr'; end
     currfname = 'kramer_IB'; 
-    currfigname = '15-network_iA';
+    currfigname = '22a-NG_iA_delta';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -15,7 +15,7 @@ function save_allfigs(currfname,currfigname)
     basepath = '.';
     % basepath = '~/figs_tosave';
     mkdir(fullfile(basepath,sp));
-    for i=[1:4]
+    for i=[1:9]
         figure(i); %ylim([0 0.175])
         %title('');
         %ylabel('');
@@ -24,10 +24,10 @@ function save_allfigs(currfname,currfigname)
         set(gcf,'Position',[0.1076    0.4544    0.7243    0.3811]);
         set(gcf,'PaperPositionMode','auto');
         %print(gcf,'-dpng','-r200',fullfile(basepath,sp,savenames{i}))
-        print(gcf,'-dpng','-r75',fullfile(basepath,sp,savenames{i}))
+        print(gcf,'-dpng','-r50',fullfile(basepath,sp,savenames{i}))
         %close
     end
-    
+    %%
     mycomment = ['Played around with network having NG iA current Fig1-2 full. Fig3-4 NMDA block.'];
     currd = pwd;
     cd ..
