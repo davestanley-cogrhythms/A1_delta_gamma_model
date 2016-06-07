9c11d3cdf3be8b7eecba66e8b54413defbf51cd4
David Stanley <davearthur.stanley@gmail.com>
2016-06-07 01:43:31 -0400

    23a-Reduced_iM_CaH: I want the bursting to be more NMDA dependent. Hence, reduce contributions from iM and CaH to allow NMDA to play a bigger role.
    With lower iM, dendrites spike like crazy. Hence, reduced poisson input lambda to 333 from 1000.
    
    Fig1 - NMDA blocked
    Fig2,3 - NMDA unblocked
    Fig4 - Increased gAMPAei to 0.2
    Fig5 - Increased gAMPAei to 0.5 (whoa! the increased inhibition broke up the synchrony!)
    Fig6,7 - Increased gAMPAee to 0.2 to compensate.
    Fig8 - Increased gGABABei (to 0.5) to produce longer spaced bursts
    Fig9 - Tried blocking gap junctions (broke up the synchrony a bit! Killed delta)
    Fig10 - Unblocked gap junctions. Tried increasing conductance from between soma and axon .3 to .4  to improve firing fidelity (produces nice, orderly delta freq bursts).
    Fig11 - Now try blocking gap …   (no change!)
    Fig12 - … or NMDA again. (still bursting).
    → It looks like we had better luck with the lower soma-axon conductance (Figs 8,9)

--- kramer_IB/kramer_IB.m
+++ kramer_IB/kramer_IB.m
@@ -31,15 +31,15 @@ Jfs2=1;
 gRAN=.015;
 ERAN=0;
 tauRAN=2;
-lambda = 1000;
+lambda = 333;
 
 
 % % % % % % % % % % % % %  Synaptic connections % % % % % % % % % % % % %  
 % compartmental connection strengths
 gsd=.2;     % IBs -> IBda,IBdb
 gds=.4;     % IBda,IBdb -> IBs
-gas=.3;     % IBa -> IBs
-gsa=.3;     % IBs -> IBa
+gas=.4;     % IBa -> IBs
+gsa=.4;     % IBs -> IBa
 
 % Gap junction connection
 ggja=0;
@@ -63,13 +63,13 @@ gGABAbie=0;
 
 
 % % Synaptic connection strengths
-gAMPAee=0.1/N;      % IBa -> IBdb, 0(.04)
+gAMPAee=0.2/N;      % IBa -> IBdb, 0(.04)
 % gNMDAee=gAMPAee/50; % uS, PY->PY, maximal NMDA conductance
-gNMDAee=10/N;
+% gNMDAee=5/N;
 % 
-gAMPAei=0.1/Nng;      % IBa -> IBdb, 0(.04)
+gAMPAei=0.5/Nng;      % IBa -> IBdb, 0(.04)
 % gNMDAei=gAMPAei/50; % uS, PY->PY, maximal NMDA conductance
-gNMDAei=10/Nng;
+% gNMDAei=5/Nng;
 % 
 gGABAaii=0.1/Nng;
 % % gGABAbii=gGABAaii/50;
@@ -77,7 +77,7 @@ gGABAbii=.3/Nng;
 % % 
 gGABAaie=0.1/N;
 % % gGABAbie=gGABAaie/50;
-gGABAbie=.35/N;
+gGABAbie=.5/N;
 
 
 
