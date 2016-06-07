7d46f88d35b34bf0b7eea2c659fdb9a10b065296
David Stanley <davearthur.stanley@gmail.com>
2016-06-07 18:12:59 -0400

    27b_testgamma Gamma off can trigger burst

--- save_allfigs.m
+++ save_allfigs.m
@@ -6,7 +6,7 @@ function save_allfigs(currfname,currfigname)
 %     if ~exist('currfname'); currfname = 'kramer_IB'; end
 %     if ~exist('currfigname'); currfigname = '3_single_comp_only_Mcurr'; end
     currfname = 'kramer_IB'; 
-    currfigname = '27a_testgamma';
+    currfigname = '27b_testgamma';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -15,7 +15,7 @@ function save_allfigs(currfname,currfigname)
     basepath = '.';
     % basepath = '~/figs_tosave';
     mkdir(fullfile(basepath,sp));
-    for i=[1:3]
+    for i=[4]
         figure(i); %ylim([0 0.175])
         %title('');
         %ylabel('');
@@ -28,7 +28,7 @@ function save_allfigs(currfname,currfigname)
         %close
     end
     
-    mycomment = ['Gamma on can shorten IB burst. (Fig1 - no FS-IB connection; Fig2 finding correct params; Fig3 actual test.)'];
+    mycomment = ['Gamma off can trigger burst'];
     currd = pwd;
     cd ..
     system('git add *');
