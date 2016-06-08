b7fdbf77b23e14d633a4dc0fbb460dea3df8f492
David Stanley <davearthur.stanley@gmail.com>
2016-06-08 01:41:51 -0400

    29a_testgamma: Turn on gamma at various times Fig1: Turn on at 513ms Fig2: Turn on at 509ms Fig3: Turn on at 520ms Fig4: Turn on at 540ms Fig5: Turn on at 560ms Fig6: Turn on at 700ms

--- save_allfigs.m
+++ save_allfigs.m
@@ -6,7 +6,7 @@ function save_allfigs(currfname,currfigname)
 %     if ~exist('currfname'); currfname = 'kramer_IB'; end
 %     if ~exist('currfigname'); currfigname = '3_single_comp_only_Mcurr'; end
     currfname = 'kramer_IB'; 
-    currfigname = '28_add_switches';
+    currfigname = '29a_testgamma';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -15,7 +15,7 @@ function save_allfigs(currfname,currfigname)
     basepath = '.';
     % basepath = '~/figs_tosave';
     mkdir(fullfile(basepath,sp));
-    for i=[1]
+    for i=[1:6]
         figure(i); %ylim([0 0.175])
         %title('');
         %ylabel('');
@@ -27,7 +27,7 @@ function save_allfigs(currfname,currfigname)
         print(gcf,'-dpng','-r50',fullfile(basepath,sp,savenames{i}))
         %close
     end
-    
+    %%
     mycomment = ['Added switches for turning different cell types on or off.'];
     currd = pwd;
     cd ..
