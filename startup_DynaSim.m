a17b1cda49af82eefec1caed935121d9fe3e6246
David Stanley <davearthur.stanley@gmail.com>
2016-06-20 17:15:46 -0400

    49a_revert47a Reverted model config to 47a.

--- startup_DynaSim.m
+++ startup_DynaSim.m
@@ -5,6 +5,13 @@ function startup_DynaSim
     % Set path to your copy of the DynaSim toolbox
     format compact
     restoredefaultpath
+    
+    set(0, 'DefaultFigureColor', 'White');
+    
+%     set(0, 'DefaultFigureColor', 'White', ...
+%     'DefaultTextFontSize', get_fontlevel(4)*scaling_factor, ...
+%     'DefaultAxesFontSize', get_fontlevel(2)*scaling_factor);
+    
     addpath((fullfile('~','src','DynaSim')));
     addpath(genpath(fullfile('~','src','DynaSim','functions')));
     addpath(genpath(fullfile('~','src','DynaSim','models')));
