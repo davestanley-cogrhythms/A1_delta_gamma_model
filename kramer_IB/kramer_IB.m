59306285d5c7d632cc1b49ee822ec262436aed99
David Stanley <davearthur.stanley@gmail.com>
2016-06-20 17:29:35 -0400

    49b_charliereport Plot isolated IB cells.

--- kramer_IB/kramer_IB.m
+++ kramer_IB/kramer_IB.m
@@ -14,11 +14,11 @@ sim_mode = 1;   % 1 - normal sim
                 
 % Cells to include in model
 include_IB = 1;
-include_FS = 1;
-include_NG = 1;
+include_FS = 0;
+include_NG = 0;
 
 % simulation controls
-tspan=[0 2000]; dt=.01; solver='euler'; % euler, rk2, rk4
+tspan=[0 1000]; dt=.01; solver='euler'; % euler, rk2, rk4
 dsfact=1; % downsample factor, applied after simulation
 
 % No noise simulation
@@ -26,7 +26,7 @@ no_noise = 0;
 
 
 % number of cells per population
-N=5;   % Number of excitatory cells
+N=1;   % Number of excitatory cells
 Nng=N;  % Number of FSNG cells
 Nfs=N;  % Number of FS cells
 
@@ -60,8 +60,8 @@ IBPPstim = 0;
 NGPPstim = 0;
 FSPPstim = 0;
 % IBPPstim = -3;
-NGPPstim = -4;
-FSPPstim = -5;
+% NGPPstim = -4;
+% FSPPstim = -5;
 
 % Steps for tuning
 %     1) Get delta oscillation
@@ -110,21 +110,21 @@ gGABAafe=0;
 
 
 % % Synaptic connection strengths
-gAMPAee=0.1/N;      % IBa -> IBdb, 0(.04)
-gNMDAee=5/N;
-% 
-gAMPAei=0.1/Nng;      % IBa -> IBdb, 0(.04)
-gNMDAei=5/Nng;
-% 
-gGABAaii=0.1/Nng;
-gGABAbii=0.3/Nng;
+% gAMPAee=0.1/N;      % IBa -> IBdb, 0(.04)
+% gNMDAee=5/N;
 % % 
-gGABAaie=0.1/N;
-gGABAbie=0.3/N;
-
-gGABAaff=0.5/Nfs;
-
-gGABAafe=.4/N;
+% gAMPAei=0.1/Nng;      % IBa -> IBdb, 0(.04)
+% gNMDAei=5/Nng;
+% % 
+% gGABAaii=0.1/Nng;
+% gGABAbii=0.3/Nng;
+% % % 
+% gGABAaie=0.1/N;
+% gGABAbie=0.3/N;
+% 
+% gGABAaff=0.5/Nfs;
+% 
+% gGABAafe=.4/N;
 
 
 % % % % % % % % % % % % % % % % % % % % % % 
--- save_allfigs.m
+++ save_allfigs.m
@@ -7,7 +7,7 @@ function save_allfigs(currfname,currfigname)
 %     if ~exist('currfigname'); currfigname = '3_single_comp_only_Mcurr'; end
     %clear all       % Clear memory for large data sets before saving figs.
     currfname = 'kr'; 
-    currfigname = '49a_revert47a';
+    currfigname = '49b_charliereport';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -17,7 +17,7 @@ function save_allfigs(currfname,currfigname)
     % basepath = '~/figs_tosave';
     mkdir(fullfile(basepath,sp));
     multiplot_on = 0;
-    for i=[1:2]
+    for i=[3]
         figure(i); %ylim([0 0.175])
         %title('');
         %ylabel('');
@@ -31,14 +31,14 @@ function save_allfigs(currfname,currfigname)
 %         print(gcf,'-dpdf',fullfile(basepath,sp,savenames{i}))
         %close
         if ~multiplot_on
-            set(gcf,'Position',[0.1076    0.4544    0.7243    0.3811]);
+%             set(gcf,'Position',[0.1076    0.4544    0.7243    0.3811]);
         end
         set(gcf,'PaperPositionMode','auto');
-        print(gcf,'-dpng','-r50',fullfile(basepath,sp,savenames{i}))
-        %print(gcf,'-dpng',fullfile(basepath,sp,savenames{i}))
+%         print(gcf,'-dpng','-r50',fullfile(basepath,sp,savenames{i}))
+        print(gcf,'-dpng',fullfile(basepath,sp,savenames{i}))
     end
-    
-    mycomment = ['Reverted model config to 47a.'];
+    %%
+    mycomment = ['Plot isolated IB cells.'];
     currd = pwd;
     cd ..
     system('git add *');
