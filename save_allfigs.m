71ff861fd76bad798684328b43682fb00dd0141b
David Stanley <davearthur.stanley@gmail.com>
2016-06-30 13:02:56 -0400

    50i_GAR_unblk Increased gAR more and also gCa and gM, in order to make cells more bursty.

--- save_allfigs.m
+++ save_allfigs.m
@@ -7,7 +7,7 @@ function save_allfigs(currfname,currfigname)
 %     if ~exist('currfigname'); currfigname = '3_single_comp_only_Mcurr'; end
     %clear all       % Clear memory for large data sets before saving figs.
     currfname = 'kr'; 
-    currfigname = '50h_GAR_unblk';
+    currfigname = '50i_hugeGAR';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -17,7 +17,7 @@ function save_allfigs(currfname,currfigname)
     % basepath = '~/figs_tosave';
     mkdir(fullfile(basepath,sp));
     multiplot_on = 1;
-    for i=[1,6]
+    for i=[1,3,[4:7]]
         figure(i); %ylim([0 0.175])
         %title('');
         %ylabel('');
@@ -41,7 +41,7 @@ function save_allfigs(currfname,currfigname)
 %         print(gcf,'-dpng',fullfile(basepath,sp,savenames{i}))
     end
     %%
-    mycomment = ['Did another sweep with AR unblocked. We can see that the SS AR state variable is affected by the resting potential.'];
+    mycomment = ['Increased gAR more and also gCa and gM, in order to make cells more bursty.'];
     currd = pwd;
     cd ..
     system('git add *');
