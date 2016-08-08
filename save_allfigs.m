821ae0d86319862cf833ac3d5b95af37ac6c06dc
David Stanley <davearthur.stanley@gmail.com>
2016-08-07 20:23:32 -0400

    57h_supra_gamma_redo Redid supraficial gamma with allowing Eleak in IBleaknoisy.txt to be controlled by parameter input.

--- save_allfigs.m
+++ save_allfigs.m
@@ -7,7 +7,7 @@ function save_allfigs(currfname,currfigname)
 %     if ~exist('currfigname'); currfigname = '3_single_comp_only_Mcurr'; end
     %clear all       % Clear memory for large data sets before saving figs.
     currfname = 'kr'; 
-    currfigname = '57g_supra_gamma';
+    currfigname = '57h_supra_gamma_redo';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -15,10 +15,10 @@ function save_allfigs(currfname,currfigname)
     sp = [sp '__' currfname '_' currfigname];
     basepath = '.';
     % basepath = '~/figs_tosave';
-    %%
+    
     mkdir(fullfile(basepath,sp));
     multiplot_on = 0;
-    for i=[1:3]
+    for i=[4:6]
         figure(i); %ylim([0 0.175])
         %title('');
         %ylabel('');
@@ -42,7 +42,7 @@ function save_allfigs(currfname,currfigname)
 %         print(gcf,'-dpng',fullfile(basepath,sp,savenames{i}))
     end
     %%
-    mycomment = ['Tuned supraficial network to produce gamma (~20-40 Hz).'];
+    mycomment = ['Redid supraficial gamma with allowing Eleak in IBleaknoisy.txt to be controlled by parameter input.'];
     currd = pwd;
     cd ..
     system('git add *');
