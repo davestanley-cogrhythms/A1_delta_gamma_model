5f67f44a9ddd883c2daac1f9433ab6499c6d1a9a
David Stanley <davearthur.stanley@gmail.com>
2016-09-23 22:28:03 -0400

    72b_sweep_iApp_nopulse Only FS cells; sweep through iApp [-5.0000   -4.0714   -3.1429   -2.2143   -1.2857   -0.3571    0.5714    1.5000]. gGABAaff=.5/Nfs.

---
+++ kramer_IB/d160923_t222653__kr_72b_sweep_iApp_nopulse/readme.txt
@@ -0,0 +1,1 @@
+72b_sweep_iApp_nopulse Only FS cells; sweep through iApp [-5.0000   -4.0714   -3.1429   -2.2143   -1.2857   -0.3571    0.5714    1.5000]. gGABAaff=.5/Nfs.
--- save_allfigs.m
+++ save_allfigs.m
@@ -7,7 +7,7 @@ function save_allfigs(currfname,currfigname)
 %     if ~exist('currfigname'); currfigname = '3_single_comp_only_Mcurr'; end
     %clear all       % Clear memory for large data sets before saving figs.
     currfname = 'kr'; 
-    currfigname = '72a_FS_only_sweep_gFSFS';
+    currfigname = '72b_sweep_iApp_nopulse';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -43,7 +43,7 @@ function save_allfigs(currfname,currfigname)
 %         print(gcf,'-dpng',fullfile(basepath,sp,savenames{i}))
     end
     
-    mycomment = ['Only FS cells; using a low level if iApp (enough to get spiking) and sweep through gFSFS.'];
+    mycomment = ['Only FS cells; sweep through iApp [-5.0000   -4.0714   -3.1429   -2.2143   -1.2857   -0.3571    0.5714    1.5000]. gGABAaff=.5/Nfs.'];
     
 
     % Write to a text file
