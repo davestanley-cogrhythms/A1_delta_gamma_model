8a2d5856e87d9f3ad39c2967a61522532100bdf1
David Stanley <davearthur.stanley@gmail.com>
2016-09-25 20:03:54 -0400

    73h_survey_PPStim_vs_gRSFS_gFSRS_0.6 RS and FS cells; PPStim [ -7.0000   -5.3333   -3.6667   -2.0000]; gRSFS [ 0.2000    0.4667    0.7333    1.0000]. gFSRS=0.6

--- save_allfigs.m
+++ save_allfigs.m
@@ -7,7 +7,7 @@ function save_allfigs(currfname,currfigname)
 %     if ~exist('currfigname'); currfigname = '3_single_comp_only_Mcurr'; end
     %clear all       % Clear memory for large data sets before saving figs.
     currfname = 'kr'; 
-    currfigname = '73g_play_gRSFS_single_sims';
+    currfigname = '73h_survey_PPStim_vs_gRSFS_gFSRS_0.6';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -18,7 +18,7 @@ function save_allfigs(currfname,currfigname)
     
     mkdir(fullfile(basepath,sp));
     multiplot_on = 0;
-    for i=[1:9]
+    for i=[1:7]
         figure(i); %ylim([0 0.175])
         %title('');
         %ylabel('');
@@ -42,15 +42,14 @@ function save_allfigs(currfname,currfigname)
         %print(gcf,'-dpdf',fullfile(basepath,sp,savenames{i}))
 %         print(gcf,'-dpng',fullfile(basepath,sp,savenames{i}))
     end
-    %%
-    mycomment = ['RS and FS cells; Played with gAMPA_rsfs (0.6 Figs1-3; 0.4 Figs4-6; 0.8 Figs 7-9). RSPPStim = -3.'];
     
+    mycomment = ['RS and FS cells; PPStim [ -7.0000   -5.3333   -3.6667   -2.0000]; gRSFS [ 0.2000    0.4667    0.7333    1.0000]. gFSRS=0.6 '];
 
     % Write to a text file
     fileID = fopen(fullfile(basepath,sp,'readme.txt'),'w');
     fprintf(fileID,[currfigname ' ' mycomment]);
     fclose(fileID);
-    %%
+    
     % Commit
     currd = pwd;
     cd ..
