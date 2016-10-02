9dc4ab3ecbd92d7f03530e485a804c6d3e5a28ff
David Stanley <davearthur.stanley@gmail.com>
2016-10-01 22:58:57 -0400

    76h_as_before_different_range2 Different range from previous. Swept NG PPSTim -1 through -3 and FS->IB through [ 0.4000    0.5000    0.6000    0.7000]. Figs 5-7 AP pulse; Figs 8-12 no AP.

---
+++ kramer_IB/d161001_t224046__kr_76h_as_before_different_range2/readme.txt
@@ -0,0 +1,1 @@
+76h_as_before_different_range2 Different range from previous. Swept NG PPSTim -1 through -3 and FS->IB through [ 0.4000    0.5000    0.6000    0.7000]. Figs 5-7 AP pulse; Figs 8-12 no AP
--- save_allfigs.m
+++ save_allfigs.m
@@ -7,7 +7,7 @@ function save_allfigs(currfname,currfigname)
 %     if ~exist('currfigname'); currfigname = '3_single_comp_only_Mcurr'; end
     %clear all       % Clear memory for large data sets before saving figs.
     currfname = 'kr'; 
-    currfigname = '76g_as_before_different_range';
+    currfigname = '76h_as_before_different_range2';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -18,7 +18,7 @@ function save_allfigs(currfname,currfigname)
     
     mkdir(fullfile(basepath,sp));
     multiplot_on = 0;
-    for i=[1:4]
+    for i=[1:12]
         figure(i); %ylim([0 0.175])
         %title('');
         %ylabel('');
@@ -44,8 +44,8 @@ function save_allfigs(currfname,currfigname)
         %print(gcf,'-dpdf',fullfile(basepath,sp,savenames{i}))
 %         print(gcf,'-dpng',fullfile(basepath,sp,savenames{i}))
     end
-    %%
-    mycomment = ['Swept NG PPSTim -2 through -5 and FS->IB through [0.2000    0.3000    0.4000    0.5000    0.6000].'];
+    
+    mycomment = ['Different range from previous. Swept NG PPSTim -1 through -3 and FS->IB through [ 0.4000    0.5000    0.6000    0.7000]. Figs 5-7 AP pulse; Figs 8-12 no AP.'];
 
     % Write to a text file
     fileID = fopen(fullfile(basepath,sp,'readme.txt'),'w');
