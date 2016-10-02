fec158361f9ebd70fbef9e6980b4cfd3c419f164
David Stanley <davearthur.stanley@gmail.com>
2016-10-01 22:16:46 -0400

    76g_as_before_different_range Swept NG PPSTim -2 through -5 and FS->IB through [0.2000    0.3000    0.4000    0.5000    0.6000].

--- save_allfigs.m
+++ save_allfigs.m
@@ -7,7 +7,7 @@ function save_allfigs(currfname,currfigname)
 %     if ~exist('currfigname'); currfigname = '3_single_comp_only_Mcurr'; end
     %clear all       % Clear memory for large data sets before saving figs.
     currfname = 'kr'; 
-    currfigname = '76f_sweep_NGPPSTim_and_FSIB';
+    currfigname = '76g_as_before_different_range';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -18,7 +18,7 @@ function save_allfigs(currfname,currfigname)
     
     mkdir(fullfile(basepath,sp));
     multiplot_on = 0;
-    for i=[1:11]
+    for i=[1:4]
         figure(i); %ylim([0 0.175])
         %title('');
         %ylabel('');
@@ -45,7 +45,7 @@ function save_allfigs(currfname,currfigname)
 %         print(gcf,'-dpng',fullfile(basepath,sp,savenames{i}))
     end
     %%
-    mycomment = ['Swept NG PPSTim -3 through -10 and FS->IB through -4 and -8. Figs1-8 no AP; 9-11 with AP.'];
+    mycomment = ['Swept NG PPSTim -2 through -5 and FS->IB through [0.2000    0.3000    0.4000    0.5000    0.6000].'];
 
     % Write to a text file
     fileID = fopen(fullfile(basepath,sp,'readme.txt'),'w');
