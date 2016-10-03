2117668eb2200d94bf271c4a31837bcaa4df60f7
David Stanley <davearthur.stanley@gmail.com>
2016-10-02 20:24:06 -0400

    78b_sweep_PPStim_IB_and_FSIB Swept through range of IB PPStim and FSIB synapse values. Seems that IB PPSTim at -3 really doesnt care what FSIB synapse is. At -4 it does.

--- save_allfigs.m
+++ save_allfigs.m
@@ -7,7 +7,7 @@ function save_allfigs(currfname,currfigname)
 %     if ~exist('currfigname'); currfigname = '3_single_comp_only_Mcurr'; end
     %clear all       % Clear memory for large data sets before saving figs.
     currfname = 'kr'; 
-    currfigname = '78a_deltagamma_adjust_PPStim_IB';
+    currfigname = '78b_sweep_PPStim_IB_and_FSIB';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -18,7 +18,7 @@ function save_allfigs(currfname,currfigname)
     
     mkdir(fullfile(basepath,sp));
     multiplot_on = 0;
-    for i=[1:10]
+    for i=[1:8]
         figure(i); %ylim([0 0.175])
         %title('');
         %ylabel('');
@@ -44,8 +44,8 @@ function save_allfigs(currfname,currfigname)
         %print(gcf,'-dpdf',fullfile(basepath,sp,savenames{i}))
 %         print(gcf,'-dpng',fullfile(basepath,sp,savenames{i}))
     end
-    %%
-    mycomment = ['Swept through range of IB PPStim values. Note that RS-FS cells actually fire at 20 Hz. Value of -3 seems good; 4 is better.'];
+    
+    mycomment = ['Swept through range of IB PPStim and FSIB synapse values. Seems that IB PPSTim at -3 really doesnt care what FSIB synapse is. At -4 it does.'];
     
     % Write to a text file
     fileID = fopen(fullfile(basepath,sp,'readme.txt'),'w');
