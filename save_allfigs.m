da1844de38bd7979ab202fed8439cd03b7702b03
David Stanley <davearthur.stanley@gmail.com>
2016-10-06 16:00:04 -0400

    79g_adjust_IBPPStim_g_FSRS Varied IB PPStim input and FS->IB through wide range [ 0.6000    0.8000    1.0000    1.2000].

--- save_allfigs.m
+++ save_allfigs.m
@@ -7,7 +7,7 @@ function save_allfigs(currfname,currfigname)
 %     if ~exist('currfigname'); currfigname = '3_single_comp_only_Mcurr'; end
     %clear all       % Clear memory for large data sets before saving figs.
     currfname = 'kr'; 
-    currfigname = '79f_settling_time_ICs';
+    currfigname = '79g_adjust_IBPPStim_g_FSRS';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -18,7 +18,7 @@ function save_allfigs(currfname,currfigname)
     
     mkdir(fullfile(basepath,sp));
     multiplot_on = 0;
-    for i=[1:4]
+    for i=[1:8]
         figure(i); %ylim([0 0.175])
         %title('');
         %ylabel('');
@@ -45,13 +45,13 @@ function save_allfigs(currfname,currfigname)
 %         print(gcf,'-dpng',fullfile(basepath,sp,savenames{i}))
     end
     %%
-    mycomment = ['Allowed 1 initial cycle of delta to give the initial conditions some settling time.'];
+    mycomment = ['Varied IB PPStim input and FS->IB through wide range [ 0.6000    0.8000    1.0000    1.2000].'];
     
     % Write to a text file
     fileID = fopen(fullfile(basepath,sp,'readme.txt'),'w');
     fprintf(fileID,[currfigname ' ' mycomment]);
     fclose(fileID);
-    
+    %%
     % Commit
     currd = pwd;
     cd ..
