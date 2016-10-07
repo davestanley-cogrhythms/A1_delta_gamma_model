42a27a2ddf5a87ad6d4c9dfcb20fb0ec0f99b32d
David Stanley <davearthur.stanley@gmail.com>
2016-10-07 00:36:24 -0400

    82b_sweep_IB_PPStim_NG_PPStim Sweeping through IB and NG PPStim values.

---
+++ kramer_IB/d161006_t233045__kr_82b_sweep_IB_PPStim_NG_PPStim/readme.txt
@@ -0,0 +1,2 @@
+82b_sweep_IB_PPStim_NG_PPStim Sweeping through IB and NG PPStim values.
+	Sweep1 Fig 7 is showing the mean FS inhibition current (Blue) versus the IB PPStim current (red).
--- save_allfigs.m
+++ save_allfigs.m
@@ -7,7 +7,7 @@ function save_allfigs(currfname,currfigname)
 %     if ~exist('currfigname'); currfigname = '3_single_comp_only_Mcurr'; end
     %clear all       % Clear memory for large data sets before saving figs.
     currfname = 'kr'; 
-    currfigname = '81a_remove_RS_NG_connection_sweep';
+    currfigname = '82b_sweep2';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -44,7 +44,7 @@ function save_allfigs(currfname,currfigname)
         %print(gcf,'-dpdf',fullfile(basepath,sp,savenames{i}))
 %         print(gcf,'-dpng',fullfile(basepath,sp,savenames{i}))
     end
-    
+    %%
     mycomment = ['Swept through IB PPStim and FS->IB synapse strength. Investigating failure of this method of getting AP pulse. Seems to be a combination of NMDA in IB cells, too slow ramp up of gamma, and insufficient gap in activity via AP pulse.'];
     
     
