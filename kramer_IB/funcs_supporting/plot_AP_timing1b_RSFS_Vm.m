155c378feac0542f3d02981a420d063b4b5b6a4b
David Stanley <davearthur.stanley@gmail.com>
2016-10-11 14:00:41 -0400

    Updated AP plotting commands

--- kramer_IB/funcs_supporting/plot_AP_timing1b_RSFS_Vm.m
+++ kramer_IB/funcs_supporting/plot_AP_timing1b_RSFS_Vm.m
@@ -1,12 +1,14 @@
 
 
-function h = plot_AP_timing1_RSFS(data)
+function h = plot_AP_timing1b_RSFS_Vm(data)
 
     ind = data.time > 1400 & data.time < 1600;
     %ind = data.time > 1100 & data.time < 1400;
 %     ind = data.time > 1000 & data.time < 1800;
     
-    hold on; h = plot(data.time(ind),mean(data.RS_FS_IBaIBdbiSYNseed_s(ind,:),2)*10,'LineWidth',5);
+    hold on; h = plot(data.time(ind),data.RS_V(ind,:)/10,'b');
+    hold on; plot(data.time(ind),data.FS_V(ind,:)/10*-1,'r');
+    hold on; plot(data.time(ind),mean(data.RS_FS_IBaIBdbiSYNseed_s(ind,:),2)*10,'LineWidth',5)
     
     hold on; plot(data.time(ind),data.RS_iPeriodicPulses_Iext(ind,:),'k','LineWidth',5)
     %xlim([1440,1560])
--- kramer_IB/funcs_supporting/plot_AP_timing2_IB.m
+++ kramer_IB/funcs_supporting/plot_AP_timing2_IB.m
@@ -1,12 +1,12 @@
 
 
-function plot_AP_timing2_IB(data)
+function h = plot_AP_timing2_IB(data)
 
-    %ind = data.time > 1400 & data.time < 1600;
+    ind = data.time > 1400 & data.time < 1600;
     %ind = data.time > 1100 & data.time < 1400;
-    ind = data.time > 1000 & data.time < 1800;
+    %ind = data.time > 1000 & data.time < 1800;
     
-    hold on; plot(data.time(ind),data.RS_FS_IBaIBdbiSYNseed_s(ind,:)*10)
+    hold on; h = plot(data.time(ind),data.RS_FS_IBaIBdbiSYNseed_s(ind,:)*10);
     hold on; plot(data.time(ind),data.IB_V(ind,:)/10)
     plot(data.time(ind),data.IB_iPeriodicPulses_Iext(ind,:),'k','LineWidth',2)
     %xlim([1440,1560])
