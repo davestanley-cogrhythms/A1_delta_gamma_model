df73e6e9318562bc190a20de56a2ea00204fab95
David Stanley <davearthur.stanley@gmail.com>
2016-10-18 19:55:54 -0400

    88a_added_RSNG_synapse Sweep FSIB and RSNG synapses. Figs 1-2 with AP; Figs 3-4 without. Good ranges for FSIB are 0.3 or 0.4. Values for RSNG synapses values 0.4 and 0.5 inhibit the AP pulse.

--- save_allfigs.m
+++ save_allfigs.m
@@ -7,7 +7,7 @@ function save_allfigs(currfname,currfigname)
 %     if ~exist('currfigname'); currfigname = '3_single_comp_only_Mcurr'; end
     %clear all       % Clear memory for large data sets before saving figs.
     currfname = 'kr'; 
-    currfigname = '87d_single_sims_NMDA_block';
+    currfigname = '88a_added_RSNG_synapse';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24','fig25','fig26','fig27','fig28'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -18,7 +18,7 @@ function save_allfigs(currfname,currfigname)
     
     mkdir(fullfile(basepath,sp));
     multiplot_on = 0;
-    for i=[3:6]
+    for i=[1:4]
         figure(i); %ylim([0 0.175])
         %title('');
         %ylabel('');
@@ -45,7 +45,7 @@ function save_allfigs(currfname,currfigname)
 %         print(gcf,'-dpng',fullfile(basepath,sp,savenames{i}))
     end
     
-    mycomment = ['Figs 3-4 without NMDA block; 5-6 with NMDA block.'];
+    mycomment = ['Sweep FSIB and RSNG synapses. Figs 1-2 with AP; Figs 3-4 without. Good ranges for FSIB are 0.3 or 0.4. Values for RSNG synapses values 0.4 and 0.5 inhibit the AP pulse.'];
     
     
     % Write to a text file
