d94f2c8f2a1affda6e815ad3e333e4dcf42cd88d
benpolletta <benpolletta@gmail.com>
2016-10-19 14:59:25 -0400

    Removing PlotFR2 (needs to be debugged) from kramer_IB; removing DynaSim path from startup_DynaSim; changing downsample_factor in kramer_IB; etc.

--- startup_DynaSim.m
+++ startup_DynaSim.m
@@ -12,13 +12,13 @@ function startup_DynaSim
 %     'DefaultTextFontSize', get_fontlevel(4)*scaling_factor, ...
 %     'DefaultAxesFontSize', get_fontlevel(2)*scaling_factor);
     
-    addpath((fullfile('~','src','DynaSim')));
-    addpath(genpath(fullfile('~','src','DynaSim','functions')));
-    addpath(genpath(fullfile('~','src','DynaSim','functions_matlab_central')));
-    addpath(genpath(fullfile('~','src','DynaSim','models')));
+%     addpath((fullfile('~','src','DynaSim')));
+%     addpath(genpath(fullfile('~','src','DynaSim','functions')));
+%     addpath(genpath(fullfile('~','src','DynaSim','functions_matlab_central')));
+%     addpath(genpath(fullfile('~','src','DynaSim','models')));
     
     % Add libdave path
-    addpath(genpath(fullfile('~','src','ds_kb2','funcs_general','lib_dav')));
+%     addpath(genpath(fullfile('~','src','ds_kb2','funcs_general','lib_dav')));
     
     
     addpath(genpath(fullfile(pwd,'supporting_funcs')));
