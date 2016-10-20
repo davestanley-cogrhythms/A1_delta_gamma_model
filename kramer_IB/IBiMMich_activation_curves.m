9c7061623a89a730738e6cd516d409275ba4871f
benp <benpolletta@gmail.com>
2016-10-19 20:39:26 -0400

    Changing activation curves in Michelle's m-current to match Gutfreund.

--- kramer_IB/IBiMMich_activation_curves.m
+++ kramer_IB/IBiMMich_activation_curves.m
@@ -1,10 +1,11 @@
 function [m_inf, tau_m] = IBiMMich_activation_curves(X)
 
-c_MaM = 1;
-c_MbM = 1;
+c_MaM = .625;
+c_MbM = 2.75;
 Qs = 3.209^2;
+halfMM = -50;
 
-aM = c_MaM.*(0.0001*Qs*(X+35)) ./ (1-exp(-(X+35)/10));
-bM = c_MbM.*(-0.0001*Qs*(X+35)) ./ (1-exp((X+35)/10));
+aM = c_MaM.*(0.0001*Qs*(X-halfMM)) ./ (1-exp(-(X-halfMM)/10));
+bM = c_MbM.*(-0.0001*Qs*(X-halfMM)) ./ (1-exp((X-halfMM)/10));
 m_inf = aM./(aM + bM);
 tau_m = 1./(aM + bM);
