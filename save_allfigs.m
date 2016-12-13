93aa4f245609b73181a37a1778e5dd98a74cf01b
David Stanley <davearthur.stanley@gmail.com>
2016-12-13 15:37:15 -0500

    101c_test_mechanism_2nd_pulse Tested what happens when RS fire or not after AP pulse. Figs1-2 RS fire. Figs3-4 RS dont fire.

--- save_allfigs.m
+++ save_allfigs.m
@@ -10,11 +10,11 @@ function save_allfigs(handles_arr)
     if nargin < 1; handles_arr = [];
     end
     
-    do_commit = 0;
+    do_commit = 1;
     
-    if isempty(handles_arr); handles_arr = 14; end
+    if isempty(handles_arr); handles_arr = 1:4; end
     currfname = 'kr'; 
-    currfigname = '101b_2D_sweep';
+    currfigname = '101c_test_mechanism_2nd_pulse';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24','fig25','fig26','fig27','fig28'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -53,7 +53,7 @@ function save_allfigs(handles_arr)
 %         print(gcf,'-dpng',fullfile(basepath,sp,savenames{i}))
     end
     
-    mycomment = ['Swept LTS firing over RS->LTS and FS-LTS synapse strength.'];
+    mycomment = ['Tested what happens when RS fire or not after AP pulse. Figs1-2 RS fire. Figs3-4 RS dont fire.'];
     
     
     % Write to a text file
