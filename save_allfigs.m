250e01322b5414ab523fbbd3df7c263362f2eead
David Stanley <davearthur.stanley@gmail.com>
2016-12-15 02:30:05 -0500

    102f_1Dsweep_unblked_Mcurrent Swept through possible gFS->LTS values. M current re-added. Updated kr_102f_readme.

--- save_allfigs.m
+++ save_allfigs.m
@@ -12,9 +12,9 @@ function save_allfigs(handles_arr)
     
     do_commit = 0;
     
-    if isempty(handles_arr); handles_arr = 5:12; end
+    if isempty(handles_arr); handles_arr = 1:6; end
     currfname = 'kr'; 
-    currfigname = '102e_2D_sweep_reblocked_Mcurent';
+    currfigname = '102f_1Dsweep_unblked_Mcurrent';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24','fig25','fig26','fig27','fig28'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -53,7 +53,7 @@ function save_allfigs(handles_arr)
 %         print(gcf,'-dpng',fullfile(basepath,sp,savenames{i}))
     end
     
-    mycomment = ['Blocked M-current; 2 different sweep ranges'];
+    mycomment = ['Swept through possible gFS->LTS values. M current re-added.'];
     
     
     % Write to a text file
