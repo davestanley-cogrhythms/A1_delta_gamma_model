6fbdf0b6e9db725d81fc5b7e23265ca8b0abc7dd
David Stanley <davearthur.stanley@gmail.com>
2017-01-23 03:22:36 -0500

    109a_test_NMDA_kineticscheme Wrote code and tested NMDA kinetics with desensitization.

---
+++ kramer_IB/test_synapse_kinetics/synapse_test.m
@@ -0,0 +1,181 @@
+
+
+
+%%
+
+% Tip: monitor all functions of a mechanism using: monitor MECHANISM.functions
+
+
+%% Test 1a - NMDA: Response to single 1 mM pulse
+    % Note - there is no sense in varying the amplitude, because a single
+    % AP will always produce roughly the same amount of NT release- this is
+    % basically Tmax, or close to 1.0 mM.
+    % I could model depletion, but I think this would mess up AMPA
+    % currents, then.
+    
+% define equations of cell model (same for E and I populations)
+eqns={ 
+  'dv/dt=@current';
+};
+stim = [1];
+onset = [10];
+offset = 11;
+
+PPstim = 0;
+PPfreq = 40; % in Hz
+
+PPonset = 30;    % ms, onset time
+PPoffset = 600;   % ms, offset time
+
+% create DynaSim specification structure
+s=[];
+s.populations(1).name='E';
+s.populations(1).size=1;
+s.populations(1).equations=eqns;
+%s.populations(1).mechanism_list={'iGABABAustin','iNMDA','iNMDADestexhe1998Markov','Vcontroller'};
+s.populations(1).mechanism_list={'iNMDA','iNMDADestexhe1998Markov','Vcontroller'};
+s.populations(1).parameters={'stim',stim,'onset',onset,'offset',offset ...
+    'PPstim',PPstim,'PPfreq',PPfreq,'PPonset',PPonset,'PPoffset',PPoffset, ...
+    };
+vary = {'E','PPstim',[.01 .1 1 10] };       % vary something randomly
+vary = {};
+
+data=SimulateModel(s,'vary',vary,'tspan',[0 800]);
+% PlotData(data);
+
+
+figure('Position',[440   143   579   655]); 
+subplot(3,1,1:2);
+for i = 1:length(data);
+    hold on; plot_normalized_max(data(i).time,[data(i).E_iNMDA_s, data(i).E_iNMDADestexhe1998Markov_o])
+end
+legend('Destexhe: Simple 2-state model', 'Destexhe: 4-state Markov model')
+ylabel('fract. open (normalized to 1)');
+xlabel('Time (ms)');
+
+subplot(3,1,3);
+plot(data(1).time,data(1).E_v);
+legend('Transmitter (mM)');
+xlabel('Time (ms)');
+ylabel('Neurotransmitter (mM)');
+
+%% Test 1c - NMDA: PPStim at 40 Hz for 500 ms
+
+eqns={ 
+  'dv/dt=@current';
+};
+stim = [0];
+onset = [10];
+offset = 500;
+
+PPstim = 1;
+PPfreq = 40; % in Hz
+
+PPonset = 50;    % ms, onset time
+PPoffset = 560;   % ms, offset time
+
+% create DynaSim specification structure
+s=[];
+s.populations(1).name='E';
+s.populations(1).size=1;
+s.populations(1).equations=eqns;
+%s.populations(1).mechanism_list={'iGABABAustin','iNMDA','iNMDADestexhe1998Markov','Vcontroller'};
+s.populations(1).mechanism_list={'iNMDA','iNMDADestexhe1998Markov','Vcontroller'};
+s.populations(1).parameters={'stim',stim,'onset',onset,'offset',offset ...
+    'PPstim',PPstim,'PPfreq',PPfreq,'PPonset',PPonset,'PPoffset',PPoffset, ...
+    };
+vary = {'E','PPstim',[.01 .1 1 10] };       % vary something randomly
+vary = {};
+
+data=SimulateModel(s,'vary',vary,'tspan',[0 800]);
+% PlotData(data);
+
+figure('Position',[440   143   579   655]); 
+subplot(3,1,1:2);
+for i = 1:length(data);
+    hold on; plot_normalized_max(data(i).time,[data(i).E_iNMDA_s, data(i).E_iNMDADestexhe1998Markov_o])
+end
+legend('Destexhe: Simple 2-state model', 'Destexhe: 4-state Markov model')
+ylabel('fract. open (normalized to 1)');
+xlabel('Time (ms)');
+
+subplot(3,1,3);
+plot(data(1).time,data(1).E_v);
+legend('Transmitter (mM)');
+xlabel('Time (ms)');
+ylabel('Neurotransmitter (mM)');
+
+
+
+%% Test 1c - NMDA: 500 ms pulse at 1 mM
+
+    
+% define equations of cell model (same for E and I populations)
+eqns={ 
+  'dv/dt=@current';
+};
+stim = [0.5];
+onset = [10];
+offset = 510;
+
+PPstim = 0.0;
+PPfreq = 40; % in Hz
+
+PPonset = 30;    % ms, onset time
+PPoffset = 600;   % ms, offset time
+
+% create DynaSim specification structure
+s=[];
+s.populations(1).name='E';
+s.populations(1).size=1;
+s.populations(1).equations=eqns;
+%s.populations(1).mechanism_list={'iGABABAustin','iNMDA','iNMDADestexhe1998Markov','Vcontroller'};
+s.populations(1).mechanism_list={'iNMDA','iNMDADestexhe1998Markov','Vcontroller'};
+s.populations(1).parameters={'stim',stim,'onset',onset,'offset',offset ...
+    'PPstim',PPstim,'PPfreq',PPfreq,'PPonset',PPonset,'PPoffset',PPoffset, ...
+    };
+vary = {'E','PPstim',[.01 .1 1 10] };       % vary something randomly
+vary = {};
+
+data=SimulateModel(s,'vary',vary,'tspan',[0 800]);
+% PlotData(data);
+
+figure('Position',[440   143   579   655]); 
+subplot(3,1,1:2);
+for i = 1:length(data);
+    hold on; plot_normalized_max(data(i).time,[data(i).E_iNMDA_s, data(i).E_iNMDADestexhe1998Markov_o])
+end
+legend('Destexhe: Simple 2-state model', 'Destexhe: 4-state Markov model')
+ylabel('fract. open (normalized to 1)');
+xlabel('Time (ms)');
+
+subplot(3,1,3);
+plot(data(1).time,data(1).E_v);
+legend('Transmitter (mM)');
+xlabel('Time (ms)');
+ylabel('Neurotransmitter (mM)');
+
+
+
+
+
+% 
+% 
+% 
+% %%
+% figure; 
+% for i = 1:length(data);
+%     hold on; plot(data(i).time,[data(i).E_iNMDA_s])
+% end
+% %%
+% figure; 
+% for i = 1:length(data);
+%     hold on; plot(data(i).time,[data(i).E_iNMDADestexhe1998Markov_o])
+% end
+% 
+% %%
+% i=3;
+% temp = 1- (data(i).E_iNMDADestexhe1998Markov_c1 + data(i).E_iNMDADestexhe1998Markov_c2 +  data(i).E_iNMDADestexhe1998Markov_d + data(i).E_iNMDADestexhe1998Markov_o);
+% figure; plot(data(i).time,[temp, data(i).E_iNMDADestexhe1998Markov_c1, data(i).E_iNMDADestexhe1998Markov_c2, data(i).E_iNMDADestexhe1998Markov_d, data(i).E_iNMDADestexhe1998Markov_o])
+% 
+% 
--- save_allfigs.m
+++ save_allfigs.m
@@ -11,10 +11,10 @@ function save_allfigs(handles_arr)
     end
     
     do_commit = 0;
-    handles_arr = 1:30;
+    handles_arr = 1:3;
     if isempty(handles_arr); handles_arr = 1:4; end
     currfname = 'kr'; 
-    currfigname = '108a_test_pulse_presets';
+    currfigname = '109a_test_NMDA_kineticscheme';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24','fig25','fig26','fig27','fig28','fig29','fig30'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -53,7 +53,7 @@ function save_allfigs(handles_arr)
 %         print(gcf,'-dpng',fullfile(basepath,sp,savenames{i}))
     end
     
-    mycomment = ['Wrote code and tested the 5 different pulse train modes.'];
+    mycomment = ['Wrote code and tested NMDA kinetics with desensitization.'];
     
     
     % Write to a text file
