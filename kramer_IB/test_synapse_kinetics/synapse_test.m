a35e34c33ac68ddda2a677c45b45d63c6841794c
David Stanley <davearthur.stanley@gmail.com>
2017-02-13 08:47:24 -0500

    109b_inc_desens Increased desensitization among NMDA channels. Figs 1-3 varied level of desens. Fig1: Rd_delta = 0*8.4*10^-3; Fig2: Rd_delta = 1*8.4*10^-3; Fig3: Rd_delta = 2*8.4*10^-3;

--- kramer_IB/test_synapse_kinetics/synapse_test.m
+++ kramer_IB/test_synapse_kinetics/synapse_test.m
@@ -59,7 +59,7 @@ legend('Transmitter (mM)');
 xlabel('Time (ms)');
 ylabel('Neurotransmitter (mM)');
 
-%% Test 1c - NMDA: PPStim at 40 Hz for 500 ms
+%% Test 1b - NMDA: PPStim at 40 Hz for 500 ms
 
 eqns={ 
   'dv/dt=@current';
@@ -133,6 +133,61 @@ s.populations(1).equations=eqns;
 s.populations(1).mechanism_list={'iNMDA','iNMDADestexhe1998Markov','Vcontroller'};
 s.populations(1).parameters={'stim',stim,'onset',onset,'offset',offset ...
     'PPstim',PPstim,'PPfreq',PPfreq,'PPonset',PPonset,'PPoffset',PPoffset, ...
+    };
+vary = {'E','PPstim',[.01 .1 1 10] };       % vary something randomly
+vary = {};
+
+data=SimulateModel(s,'vary',vary,'tspan',[0 800]);
+% PlotData(data);
+
+figure('Position',[440   143   579   655]); 
+subplot(3,1,1:2);
+for i = 1:length(data);
+    hold on; plot_normalized_max(data(i).time,[data(i).E_iNMDA_s, data(i).E_iNMDADestexhe1998Markov_o])
+end
+legend('Destexhe: Simple 2-state model', 'Destexhe: 4-state Markov model')
+ylabel('fract. open (normalized to 1)');
+xlabel('Time (ms)');
+
+subplot(3,1,3);
+plot(data(1).time,data(1).E_v);
+legend('Transmitter (mM)');
+xlabel('Time (ms)');
+ylabel('Neurotransmitter (mM)');
+
+
+
+%% Test 1d - NMDA: 500 ms pulse at 1 mM; increased desensitization
+
+    
+% define equations of cell model (same for E and I populations)
+eqns={ 
+  'dv/dt=@current';
+};
+stim = [0.5];
+onset = [10];
+offset = 510;
+
+PPstim = 0.0;
+PPfreq = 40; % in Hz
+
+PPonset = 30;    % ms, onset time
+PPoffset = 600;   % ms, offset time
+
+Rd_delta = 2*8.4*10^-3;
+Rd = 8.4*10^-3 - Rd_delta;
+Rr = 6.8*10^-3 + Rd_delta;
+
+% create DynaSim specification structure
+s=[];
+s.populations(1).name='E';
+s.populations(1).size=1;
+s.populations(1).equations=eqns;
+%s.populations(1).mechanism_list={'iGABABAustin','iNMDA','iNMDADestexhe1998Markov','Vcontroller'};
+s.populations(1).mechanism_list={'iNMDA','iNMDADestexhe1998Markov','Vcontroller'};
+s.populations(1).parameters={'stim',stim,'onset',onset,'offset',offset ...
+    'PPstim',PPstim,'PPfreq',PPfreq,'PPonset',PPonset,'PPoffset',PPoffset, ...
+    'Rd ', Rd, 'Rr', Rr, ...
     };
 vary = {'E','PPstim',[.01 .1 1 10] };       % vary something randomly
 vary = {};
