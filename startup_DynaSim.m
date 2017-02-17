6081b1199421e5bf02edd0b0db9eb8a7f1765483
David Stanley <davearthur.stanley@gmail.com>
2017-02-17 17:11:41 -0500

    Edited startup_Dynasim.

--- startup_DynaSim.m
+++ startup_DynaSim.m
@@ -12,10 +12,10 @@ function startup_DynaSim
 %     'DefaultTextFontSize', get_fontlevel(4)*scaling_factor, ...
 %     'DefaultAxesFontSize', get_fontlevel(2)*scaling_factor);
     
-    addpath((fullfile('~','src','DynaSim')));
-    addpath(genpath(fullfile('~','src','DynaSim','functions')));
-    addpath(genpath(fullfile('~','src','DynaSim','functions_matlab_central')));
-    addpath(genpath(fullfile('~','src','DynaSim','models')));
+    addpath(genpath(fullfile('~','src','DynaSimSherfey')));
+%     addpath(genpath(fullfile('~','src','DynaSim','functions')));
+%     addpath(genpath(fullfile('~','src','DynaSim','functions_matlab_central')));
+%     addpath(genpath(fullfile('~','src','DynaSim','models')));
     
     % Add libdave path
     addpath(genpath(fullfile('~','src','ds_kb3','funcs_general','lib_dav')));
