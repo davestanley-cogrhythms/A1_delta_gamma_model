21ab913ae0cafca26274dec6bc6524062687b9a9
benpolletta <benpolletta@gmail.com>
2017-02-21 12:34:57 -0500

    Whitespace, and fixes to kramer_IB_function_mode.

--- kramer_IB/kramer_IB_function_mode.m
+++ kramer_IB/kramer_IB_function_mode.m
@@ -4,6 +4,8 @@ if nargin < 1; sim_struct = []; end
 if isempty(sim_struct); sim_struct = struct; end
 
 Today = datestr(datenum(date),'yy-mm-dd');
+mkdir(Today);
+
 savepath = fullfile('Figs_Ben',Today);
 mkdir(savepath);
 
@@ -11,8 +13,8 @@ function_mode = 1;
 
 kramer_IB
 
-%unpack_sim_struct
-vars_pull(sim_struct);
+unpack_sim_struct
+% vars_pull(sim_struct);
 
 include_kramer_IB_populations;
 
@@ -30,14 +32,14 @@ else
     
     data=SimulateModel(sim_spec,'tspan',tspan,'dt',dt,'downsample_factor',dsfact,'solver',solver,'coder',0,...
         'random_seed',random_seed,'vary',vary,'verbose_flag',verbose_flag,'parallel_flag',parallel_flag,...
-        'compile_flag',compile_flag,'save_data_flag',save_data_flag,'study_dir',name);
+        'compile_flag',compile_flag);
 
 end
 
 PlotData(data)
 
-% save_as_pdf(gcf, fullfile(savepath,name))
+save_as_pdf(gcf, fullfile(savepath, name))
 
-% save(fullfile(Today, name, '_sim_struct.mat'), 'sim_struct');
+save(fullfile(Today, [name, '_sim_struct.mat']), 'sim_struct');
 
 end
