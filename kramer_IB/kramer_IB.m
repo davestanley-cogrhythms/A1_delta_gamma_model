37b0c7007a926d0c8a690eff8d58730e4e040036
David Stanley <davearthur.stanley@gmail.com>
2017-02-21 15:57:45 -0500

    Enabled RS-FS-LTS cell network.

--- kramer_IB/kramer_IB.m
+++ kramer_IB/kramer_IB.m
@@ -38,7 +38,7 @@ end
 plot_on = 1;
 save_plots = 0;
 visible_flag = 'on';
-compile_flag = 0;
+compile_flag = 1;
 parallel_flag = 0;
 cluster_flag = 0;
 save_data_flag = 0;
@@ -59,11 +59,11 @@ no_synapses = 0;
 NMDA_block = 0;
 
 % % % % % Cells to include in model
-include_IB = 1;
+include_IB = 0;
 include_RS = 1;
 include_FS = 1;
 include_LTS = 1;
-include_NG = 1;
+include_NG = 0;
 include_supRS = 0;
 include_supFS = 0;
 include_deepRS = 0;
