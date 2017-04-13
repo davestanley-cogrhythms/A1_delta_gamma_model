5f57b6616a87dc815c1239dfa95248877552e858
David Stanley <davearthur.stanley@gmail.com>
2017-04-12 21:29:15 -0400

    119g_sweep2D_gFSFS_gRSFS Additional sweep of previous with broader range. 	Rebase: Redid kr_119g sweep with axes showing synaptic totals. 	Rebase: Deleted data.mat

--- save_allfigs_Dave.m
+++ save_allfigs_Dave.m
@@ -15,7 +15,7 @@ function save_allfigs_Dave(handles_arr)
     handles_arr = 1;
     if isempty(handles_arr); handles_arr = 1:4; end
     currfname = 'kr'; 
-    currfigname = '119f_sweep2D_gFSFS_gRSFS';
+    currfigname = '119g_sweep2D_gFSFS_gRSFS';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24','fig25','fig26','fig27','fig28','fig29','fig30'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -61,7 +61,7 @@ function save_allfigs_Dave(handles_arr)
     end
     
 
-    mycomment = ['Hard to get FS cells to not fire on second pulse.'];
+    mycomment = ['Additional sweep of previous with broader range.'];
     
     % Write to a text file
     fileID = fopen(fullfile(basepath,sp,'readme.txt'),'w');
