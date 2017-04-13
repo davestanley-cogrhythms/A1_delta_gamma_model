988d94d99db49a670ff743d00029b773de1a117d
David Stanley <davearthur.stanley@gmail.com>
2017-04-13 15:17:44 -0400

    Updated save_allfigs_Dave to save also .m file, study info, and possibly other pre-generated figures.

--- save_allfigs_Dave.m
+++ save_allfigs_Dave.m
@@ -12,10 +12,10 @@ function save_allfigs_Dave(handles_arr)
     
     do_commit = 0;
     supersize_me = 0;
-    handles_arr = 1;
+    handles_arr = 1:3;
     if isempty(handles_arr); handles_arr = 1:4; end
     currfname = 'kr'; 
-    currfigname = '119g_sweep2D_gFSFS_gRSFS';
+    currfigname = '120a_single_sim';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24','fig25','fig26','fig27','fig28','fig29','fig30'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -27,6 +27,7 @@ function save_allfigs_Dave(handles_arr)
     mkdir(fullfile(basepath,sp));
     multiplot_on = 0;
     
+    % Save plots
     for i=handles_arr
         %figure(i); %ylim([0 0.175])
         %title('');
@@ -54,14 +55,34 @@ function save_allfigs_Dave(handles_arr)
         end
         set(i,'PaperPositionMode','auto');
         %print(gcf,'-dpng','-r100',fullfile(basepath,sp,savenames{i}));
-        tic; print(i,'-dpng','-r50','-opengl',fullfile(basepath,sp,savenames{i}));toc
+        tic; print(i,'-dpng','-r100','-opengl',fullfile(basepath,sp,savenames{i}));toc
         %tic; screencapture(gcf,[],fullfile(basepath,sp,[savenames{ina} '.png']));toc
         %print(gcf,'-dpdf',fullfile(basepath,sp,savenames{i}))
 %         print(gcf,'-dpng',fullfile(basepath,sp,savenames{i}))
     end
     
+    % Save spec file
+    save(fullfile(basepath,sp,'spec.mat'),'spec');
+    
+    % Save .m file
+    zip(fullfile(basepath,sp,'kramer_IB.zip'),{'kramer_IB.m','include_kramer_IB_populations.m','include_kramer_IB_synapses.m'});
+    
+    % Copy study info file
+    copyfile(fullfile(study_dir,'studyinfo.mat'),fullfile(basepath,sp));
+    
+    % Copy raw plots if not empty
+    if exist(fullfile(study_dir,'plots'),'dir')
+        % copyfile(fullfile(study_dir,'plots'),fullfile(basepath,sp));
+    end
+    
+    % Copy saved plots if not empty
+    if exist('save_path','var')
+        if exist(save_path,'dir')
+            movefile(savepath,fullfile(basepath,sp));
+        end
+    end
 
-    mycomment = ['Additional sweep of previous with broader range.'];
+    mycomment = ['Do single simulation with seemingly optimal parameters.'];
     
     % Write to a text file
     fileID = fopen(fullfile(basepath,sp,'readme.txt'),'w');
