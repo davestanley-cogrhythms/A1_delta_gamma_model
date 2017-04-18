e8ae75f0cc5a616e7ca0611e030a97a13d406f3a
David Stanley <davearthur.stanley@gmail.com>
2017-04-18 15:33:50 -0400

    125c_sweep3D_LTS_PPStim_gRSLTS_gFSLTS Swept over LTS_PPstim_gsyn, gRSLTS, gFSLTS

--- save_allfigs_Dave.m
+++ save_allfigs_Dave.m
@@ -10,12 +10,12 @@ function save_allfigs_Dave(handles_arr)
 %     if nargin < 1; handles_arr = [];
 %     end
     
-    do_commit = 1;
+    do_commit = 0;
     supersize_me = 0;
-    handles_arr = 1:2;
+    handles_arr = 1:1;
     if isempty(handles_arr); handles_arr = 1:4; end
     currfname = 'kr'; 
-    currfigname = '125b_added_PPStim_2_LTS';
+    currfigname = '125d_sweep3D_redo';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24','fig25','fig26','fig27','fig28','fig29','fig30'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -84,7 +84,7 @@ function save_allfigs_Dave(handles_arr)
         end
     end
     %
-    mycomment = ['Added PP Stim going to LTS cells. Also added synaptic hetero to all PPStim mechs'];
+    mycomment = ['Swept over LTS_PPstim_gsyn, gRSLTS, gFSLTS'];
     
     % Write to a text file
     fileID = fopen(fullfile(basepath,sp,'readme.txt'),'w');
