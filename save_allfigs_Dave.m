9cf1f2af3497574ead5068f7a195f715923544de
David Stanley <davearthur.stanley@gmail.com>
2017-04-19 19:44:06 -0400

    127c_sweep2D_LTS_PPStim_gFSLTS Swept through gFSLTS And LTS_PPStim

--- save_allfigs_Dave.m
+++ save_allfigs_Dave.m
@@ -15,7 +15,7 @@ function save_allfigs_Dave(handles_arr)
     handles_arr = 1:1;
     if isempty(handles_arr); handles_arr = 1:4; end
     currfname = 'kr'; 
-    currfigname = '127b_sweep2D_RSFS_PPStim';
+    currfigname = '127c_sweep2D_LTS_PPStim_gFSLTS';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24','fig25','fig26','fig27','fig28','fig29','fig30'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -84,7 +84,7 @@ function save_allfigs_Dave(handles_arr)
         end
     end
     %
-    mycomment = ['Swept through RS and FS PP Stim values'];
+    mycomment = ['Swept through gFSLTS And LTS_PPStim'];
     
     % Write to a text file
     fileID = fopen(fullfile(basepath,sp,'readme.txt'),'w');
