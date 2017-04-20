40f8dfbf497537d76b9ca27133e1106505ac0df1
David Stanley <davearthur.stanley@gmail.com>
2017-04-20 11:50:47 -0400

    Restored kramer_IB to more default state.

--- kramer_IB/kramer_IB.m
+++ kramer_IB/kramer_IB.m
@@ -13,7 +13,7 @@ addpath(genpath(fullfile(pwd,'funcs_Ben')));
 % these master parameters first!
 
 tspan=[0 700];
-sim_mode = 9;               % % % % Choice normal sim (sim_mode=1) or parallel sim options
+sim_mode = 1;               % % % % Choice normal sim (sim_mode=1) or parallel sim options
                             % 2 - Vary I_app in deep RS cells
                             % 9 - sim study FS-RS circuit vary RS stim
                             % 10 - Vary iPeriodicPulses in all cells
@@ -70,7 +70,7 @@ do_jason_sPING_syn = 0;
 % % % % % Display options
 plot_on = 0;
 visible_flag = 'on';
-compile_flag = 0;
+compile_flag = 1;
 parallel_flag = double(any(sim_mode == [9:14]));            % Sim_modes 9 - 14 are for Dave's vary simulations. Want par mode on for these.
 cluster_flag = 0;
 save_data_flag = 0;
@@ -97,13 +97,13 @@ dsfact=max(round(0.1/dt),1); % downsample factor, applied after simulation
 
 % % % % % Simulation switches
 no_noise = 0;
-no_synapses = 1;
+no_synapses = 0;
 NMDA_block = 0;
 
 % % % % % Cells to include in model
 include_IB = 0;
-include_RS = 0;
-include_FS = 0;
+include_RS = 1;
+include_FS = 1;
 include_LTS = 1;
 include_NG = 0;
 include_supRS = 0;
