b5b3e151c0f54f3ad824670991bd0a59ebdd38ea
benp <benpolletta@gmail.com>
2017-05-01 14:49:49 -0400

    getPeriodicPulseFastBensVersion: removing commmented text.

--- kramer_IB/funcs_Ben/getPeriodicPulseFastBensVersion.m
+++ kramer_IB/funcs_Ben/getPeriodicPulseFastBensVersion.m
@@ -1,23 +1,5 @@
 function s3 = getPeriodicPulseFastBensVersion(freq,width,shift,T,dt,onset,offset,Npop,kernel_type,width2_rise,plot_demo_on)
 
-% Comment this out because it gets confusing.
-% if nargin < 5
-%     onset = 0;
-%     offset = Inf;
-% end
-% if nargin < 7
-%     ap_pulse_num = 0;
-%     ap_pulse_delay = 0;
-% end
-% if nargin < 10
-%     kernel_type = 1;  % Options for kernel type are:
-%                                 % 1-Gaussian
-%                                 % 2-Double exponential - width is decay time; width2 is rise
-% end
-% if nargin < 11
-%     width2_rise = 0.5; % 0.5 ms by default
-% end
-
 % plot_demo_on = 1;  % Plot if desired
 
 % Build train of delta functions, spaced with frequency "freq"
