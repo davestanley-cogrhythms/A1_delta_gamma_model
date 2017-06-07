06501fdb949ef87a311595f15863c6445c9bfc3d
David Stanley <davearthur.stanley@gmail.com>
2017-06-07 18:17:38 -0400

    137g_case8_Jlts_gRSLTS Swept through Jlts and also gRS_LTS to find good level of stim for LTS cells.

--- save_allfigs_Dave.m
+++ save_allfigs_Dave.m
@@ -15,7 +15,7 @@ function save_allfigs_Dave(handles_arr)
     handles_arr = [1:1];
     if isempty(handles_arr); handles_arr = 1:4; end
     currfname = 'kr'; 
-    currfigname = '137f_case8_Jlts';
+    currfigname = '137g_case8_Jlts_gRSLTS';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24','fig25','fig26','fig27','fig28','fig29','fig30'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -84,7 +84,7 @@ function save_allfigs_Dave(handles_arr)
         end
     end
     %
-    mycomment = ['Swept through inhib feedback from LTS to RS and FS.'];
+    mycomment = ['Swept through Jlts and also gRS_LTS to find good level of stim for LTS cells.'];
     
     % Write to a text file
     fileID = fopen(fullfile(basepath,sp,'readme.txt'),'w');
