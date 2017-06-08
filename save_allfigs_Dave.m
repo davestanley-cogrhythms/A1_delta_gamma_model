9c2e4e650db557c0785781e9b9d604ca8092ac58
David Stanley <davearthur.stanley@gmail.com>
2017-06-08 15:46:24 -0400

    138c_sweep_gLTSRS_2 Redo prev.

--- save_allfigs_Dave.m
+++ save_allfigs_Dave.m
@@ -15,7 +15,7 @@ function save_allfigs_Dave(handles_arr)
     handles_arr = [1:6];
     if isempty(handles_arr); handles_arr = 1:4; end
     currfname = 'kr'; 
-    currfigname = '138a_single_sweeps';
+    currfigname = '138c_sweep_gLTSRS_2';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24','fig25','fig26','fig27','fig28','fig29','fig30'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -84,7 +84,7 @@ function save_allfigs_Dave(handles_arr)
         end
     end
     %
-    mycomment = ['Did a bunch of single sweeps with LTS gM set to 8 and Jlts at 1.2.'];
+    mycomment = ['Redo prev.'];
     
     % Write to a text file
     fileID = fopen(fullfile(basepath,sp,'readme.txt'),'w');
