b58c9fa836089225c08a40aa8441d3161eb8b6b5
David Stanley <davearthur.stanley@gmail.com>
2017-06-08 20:25:51 -0400

    139a_singlesim Single sim with feedback from LTS to RS cells now included.

--- kramer_IB/kramer_IB.m
+++ kramer_IB/kramer_IB.m
@@ -13,7 +13,7 @@ addpath(genpath(fullfile(pwd,'funcs_Ben')));
 % these master parameters first!
 
 tspan=[0 700];
-sim_mode = 9;               % % % % Choice normal sim (sim_mode=1) or parallel sim options
+sim_mode = 1;               % % % % Choice normal sim (sim_mode=1) or parallel sim options
                             % 2 - Vary I_app in deep RS cells
                             % 9 - sim study FS-RS circuit vary RS stim
                             % 10 - Vary iPeriodicPulses in all cells
@@ -382,7 +382,7 @@ if ~no_synapses
     
     gAMPA_rsLTS = 0.6/Nrs;                 % RS -> LTS
     %     gNMDA_rsLTS = 0/Nrs;              % RS -> LTS NMDA
-%     gGABAa_LTSrs = .5/Nlts;                  % LTS -> RS
+    gGABAa_LTSrs = 1.25/Nlts;                  % LTS -> RS
     
     gGABAa_fsLTS = 1.0/Nfs;                  % FS -> LTS
 %     gGABAa_LTSfs = 0.2/Nlts;                % LTS -> FS
