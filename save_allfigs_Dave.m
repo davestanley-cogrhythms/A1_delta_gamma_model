9c836967811c11607e9eb807c07e560aa417cf9d
David Stanley <davearthur.stanley@gmail.com>
2017-06-10 18:09:50 -0400

    140c_redo_prev Redid prev additional sims.

--- save_allfigs_Dave.m
+++ save_allfigs_Dave.m
@@ -12,10 +12,10 @@ function save_allfigs_Dave(handles_arr)
     
     do_commit = 0;
     supersize_me = 0;
-    handles_arr = [1:6];
+    handles_arr = [7:12];
     if isempty(handles_arr); handles_arr = 1:4; end
     currfname = 'kr'; 
-    currfigname = '140b_single_RSFSLTS';
+    currfigname = '140c_redo_prev';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24','fig25','fig26','fig27','fig28','fig29','fig30'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -84,7 +84,7 @@ function save_allfigs_Dave(handles_arr)
         end
     end
     %
-    mycomment = ['Did multiple random seed sweeps with RS FS LTS network. Increased gLTS->RS slightly to 1.5. Decreased gFS->FS to 0.3. Also added dsPlot_with_AP_line function.'];
+    mycomment = ['Redid prev additional sims.'];
     
     % Write to a text file
     fileID = fopen(fullfile(basepath,sp,'readme.txt'),'w');
