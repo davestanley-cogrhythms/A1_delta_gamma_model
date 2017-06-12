b6d882b4254e8bf6973002487a807e17b9669a12
David Stanley <davearthur.stanley@gmail.com>
2017-06-12 00:35:24 -0400

    144a_characterization Characterization of spontaneous and 30 Hz responses. Figs 1-2 40 Hz. Figs 3-5 spontaneous. Figs 6-8 30 Hz

--- save_allfigs_Dave.m
+++ save_allfigs_Dave.m
@@ -15,10 +15,10 @@ function save_allfigs_Dave(handles_arr)
     
     do_commit = 0;
     supersize_me = 0;
-    handles_arr = [1:4];
+    handles_arr = [1:8];
     if isempty(handles_arr); handles_arr = 1:4; end
     currfname = 'kr'; 
-    currfigname = '143d_LTS_gM_2.5';
+    currfigname = '144a_characterization';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24','fig25','fig26','fig27','fig28','fig29','fig30'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -87,7 +87,7 @@ function save_allfigs_Dave(handles_arr)
         end
     end
     %
-    mycomment = ['Found a good balance'];
+    mycomment = ['Characterization of spontaneous and 30 Hz responses. Figs 1-2 40 Hz. Figs 3-5 spontaneous. Figs 6-8 30 Hz'];
     
     % Write to a text file
     fileID = fopen(fullfile(basepath,sp,'readme.txt'),'w');
