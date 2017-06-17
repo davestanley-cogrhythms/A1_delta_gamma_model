37089bec555ae80da22be533be620dcd9dbc7889
David Stanley <davearthur.stanley@gmail.com>
2017-06-16 21:28:45 -0400

    150f_shuffle Shuffle round.

--- save_allfigs_Dave.m
+++ save_allfigs_Dave.m
@@ -15,10 +15,10 @@ function save_allfigs_Dave(handles_arr)
     
     do_commit = 1;
     supersize_me = 0;
-    handles_arr = [1:6];
+    handles_arr = [1:5];
     if isempty(handles_arr); handles_arr = 1:4; end
     currfname = 'kr'; 
-    currfigname = '150e_fullnet_sweep_NGRS';
+    currfigname = '150f_shuffle';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24','fig25','fig26','fig27','fig28','fig29','fig30'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -87,7 +87,7 @@ function save_allfigs_Dave(handles_arr)
         end
     end
     %
-    mycomment = ['Swept through NGRS values. Still seeing weird feedback effects.'];
+    mycomment = ['Shuffle round.'];
     
     % Write to a text file
     fileID = fopen(fullfile(basepath,sp,'readme.txt'),'w');
