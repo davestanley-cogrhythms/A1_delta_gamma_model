9dc7723c892e39c5bd1d3edecad59d80e524a4c0
David Stanley <davearthur.stanley@gmail.com>
2017-06-18 20:59:28 -0400

    152c_inc_gNGRS Increased gNGRS to get better inhibition of RS cells following initial stim. Swept gIBRS for both NMDA and AMPA

--- save_allfigs_Dave.m
+++ save_allfigs_Dave.m
@@ -15,10 +15,10 @@ function save_allfigs_Dave(handles_arr)
     
     do_commit = 1;
     supersize_me = 0;
-    handles_arr = [1:5];
+    handles_arr = [1:1];
     if isempty(handles_arr); handles_arr = 1:2; end
     currfname = 'kr'; 
-    currfigname = '152b_add_gLTSIB';
+    currfigname = '152c_inc_gNGRS';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24','fig25','fig26','fig27','fig28','fig29','fig30'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -87,7 +87,7 @@ function save_allfigs_Dave(handles_arr)
         end
     end
     %
-    mycomment = ['Swept over gLTS IB synapse strength.'];
+    mycomment = ['Increased gNGRS to get better inhibition of RS cells following initial stim. Swept gIBRS for both NMDA and AMPA'];
     
     % Write to a text file
     fileID = fopen(fullfile(basepath,sp,'readme.txt'),'w');
