3ae4b3d650ff1c8353669e70638ca9daf8da407c
David Stanley <davearthur.stanley@gmail.com>
2017-06-19 05:48:52 -0400

    154h_inc_RSPPStim PPShift with a stronger PPStim value of 2.5 now. Still no big shifts

--- save_allfigs_Dave.m
+++ save_allfigs_Dave.m
@@ -18,7 +18,7 @@ function save_allfigs_Dave(handles_arr)
     handles_arr = [1:5];
     if isempty(handles_arr); handles_arr = 1:2; end
     currfname = 'kr'; 
-    currfigname = '154g_PPShift';
+    currfigname = '154h_inc_RSPPStim';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24','fig25','fig26','fig27','fig28','fig29','fig30'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -87,7 +87,7 @@ function save_allfigs_Dave(handles_arr)
         end
     end
     %
-    mycomment = ['PPShift with a stronger PPStim value of 1.5'];
+    mycomment = ['PPShift with a stronger PPStim value of 2.5 now. Still no big shifts'];
     
     % Write to a text file
     fileID = fopen(fullfile(basepath,sp,'readme.txt'),'w');
