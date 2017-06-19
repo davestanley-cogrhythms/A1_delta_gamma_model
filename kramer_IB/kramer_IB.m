9c6c6f723a72fcae71f13d09a58764b25e0a586d
David Stanley <davearthur.stanley@gmail.com>
2017-06-19 01:53:24 -0400

    Reset to default settings.

--- kramer_IB/kramer_IB.m
+++ kramer_IB/kramer_IB.m
@@ -209,8 +209,8 @@ Jng2=1;   %
 JRS1 = -1.5; % RS cells
 JRS2 = -1.5; %
 if include_NG
-    JRS1 = -1.9; % RS cells
-    JRS2 = -1.9; %
+    JRS1 = -2.1; % RS cells
+    JRS2 = -2.1; %
 end
 Jfs=1;    % FS cells
 Jlts1=-2.5; % LTS cells
@@ -624,7 +624,7 @@ switch pulse_mode
         deepRSPPstim = 0;
         IB_PP_gNMDA = 0;
     case 1                  % Gamma stimulation (with aperiodicity)
-        PPfreq = 30; % in Hz
+        PPfreq = 40; % in Hz
         PPtauDx = tauAMPAd+jitter_fall; % in ms        % Broaden by fixed amount due to presynaptic jitter
         PPshift = 0; % in ms
         PPonset = 450;    % ms, onset time
@@ -632,7 +632,7 @@ switch pulse_mode
         %PPoffset=270;   % ms, offset time
         ap_pulse_num = round(tspan(end)/(1000/PPfreq))-10;     % The pulse number that should be delayed. 0 for no aperiodicity.
         ap_pulse_delay = 11;                        % ms, the amount the spike should be delayed. 0 for no aperiodicity.
-        ap_pulse_num = 0;  % ms, the amount the spike should be delayed. 0 for no aperiodicity.
+%         ap_pulse_num = 0;  % ms, the amount the spike should be delayed. 0 for no aperiodicity.
         pulse_train_preset = 1;     % Preset number to use for manipulation on pulse train (see getDeltaTrainPresets.m for details; 0-no manipulation; 1-aperiodic pulse; etc.)
         PPtauRx = tauAMPAr+jitter_rise;      % Broaden by fixed amount due to presynaptic jitter
         kernel_type = 1;
