3ac1b249b06016b97f6247ac56ef8ac7551fee98
David Stanley <davearthur.stanley@gmail.com>
2017-07-17 20:57:10 -0400

    156b_sweep_gFSIB2 Swept through gFSIB GABA A synapse strength. Redo prev, different range

--- save_allfigs_Dave.m
+++ save_allfigs_Dave.m
@@ -13,12 +13,12 @@ function save_allfigs_Dave(handles_arr)
 
     [a,b] = fileparts(pwd); if ~strcmp(b,'kramer_IB'); error('Must be in kramer_IB working directory to run.'); end
     
-    do_commit = 0;
+    do_commit = 1;
     supersize_me = 0;
-    handles_arr = [1];
+    handles_arr = [1:7];
     if isempty(handles_arr); handles_arr = 1:2; end
     currfname = 'kr'; 
-    currfigname = '156a_sweep_gFSIB';
+    currfigname = '156b_sweep_gFSIB2';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24','fig25','fig26','fig27','fig28','fig29','fig30'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -87,7 +87,7 @@ function save_allfigs_Dave(handles_arr)
         end
     end
     %
-    mycomment = ['Swept through gFSIB GABA A synapse strength. Too high'];
+    mycomment = ['Swept through gFSIB GABA A synapse strength. Redo prev, different range'];
     
     % Write to a text file
     fileID = fopen(fullfile(basepath,sp,'readme.txt'),'w');
