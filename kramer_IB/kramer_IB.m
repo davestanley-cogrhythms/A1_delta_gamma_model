d80e146604005ff3003e168ec537bc8f68c6813d
David Stanley <davearthur.stanley@gmail.com>
2017-08-10 02:07:04 -0400

    158b_as_before_NO_AP Generated some figures for the paper. Using FS inhib mode.

--- kramer_IB/kramer_IB.m
+++ kramer_IB/kramer_IB.m
@@ -639,7 +639,7 @@ switch pulse_mode
         %PPoffset=270;   % ms, offset time
         ap_pulse_num = round(tspan(end)/(1000/PPfreq))-10;     % The pulse number that should be delayed. 0 for no aperiodicity.
         ap_pulse_delay = 11;                        % ms, the amount the spike should be delayed. 0 for no aperiodicity.
-%         ap_pulse_num = 0;  % ms, the amount the spike should be delayed. 0 for no aperiodicity.
+        ap_pulse_num = 0;  % ms, the amount the spike should be delayed. 0 for no aperiodicity.
         pulse_train_preset = 1;     % Preset number to use for manipulation on pulse train (see getDeltaTrainPresets.m for details; 0-no manipulation; 1-aperiodic pulse; etc.)
         PPtauRx = tauAMPAr+jitter_rise;      % Broaden by fixed amount due to presynaptic jitter
         kernel_type = 1;
@@ -919,6 +919,7 @@ if plot_on
             
             
             
+            dsPlot2(data,'plot_type','raster','xlims',[400 1500]);
             dsPlot2(data,'population','IB','variable','/NMDA_s|NG_GABA_gTH/','xlims',[400 1500],'do_mean',true,'force_last','variable')
             dsPlot2(data,'population','/RS|LTS/','variable','Mich','xlims',[400 1500],'do_mean',true)
             
