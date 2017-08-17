e54a2e36f3f9e15f0d3ad6a642817f94fe8d6246
David Stanley <davearthur.stanley@gmail.com>
2017-08-16 20:28:27 -0400

    161b_add_deepFS_AP As before. Driven with AP.

--- save_allfigs_Dave.m
+++ save_allfigs_Dave.m
@@ -15,10 +15,10 @@ function save_allfigs_Dave(handles_arr)
     
     do_commit = 1;
     supersize_me = 0;
-    handles_arr = [1:2];
+    handles_arr = [3:4];
     if isempty(handles_arr); handles_arr = 1:2; end
     currfname = 'kr'; 
-    currfigname = '161a_add_deepFS_spontaneous';
+    currfigname = '161b_add_deepFS_AP';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24','fig25','fig26','fig27','fig28','fig29','fig30'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -87,7 +87,7 @@ function save_allfigs_Dave(handles_arr)
         end
     end
     %
-    mycomment = ['Added a deep FS cell. Spontaneous activity simulation.'];
+    mycomment = ['As before. Driven with AP.'];
     
     % Write to a text file
     fileID = fopen(fullfile(basepath,sp,'readme.txt'),'w');
