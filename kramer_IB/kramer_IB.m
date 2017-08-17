82c77f71b0e03eb98a527baecdfc2e5b8c91225c
David Stanley <davearthur.stanley@gmail.com>
2017-08-17 01:01:34 -0400

    161c_add_deepFS_noAP As before. Driven with no AP. Figs [1,2] and [3,4] are exact same sim parameters, but different random seed!

--- kramer_IB/kramer_IB.m
+++ kramer_IB/kramer_IB.m
@@ -85,7 +85,7 @@ save_data_flag = 0;
 save_results_flag = double(~isempty(plot_options));         % If plot_options is supplied, save the results.
 verbose_flag = 1;
 random_seed = 'shuffle';
-random_seed = 2;
+% random_seed = 2;
 study_dir = ['study_' sp];
 % study_dir = [];
 % study_dir = ['study_dave'];
@@ -644,7 +644,7 @@ switch pulse_mode
         %PPoffset=270;   % ms, offset time
         ap_pulse_num = round(tspan(end)/(1000/PPfreq))-10;     % The pulse number that should be delayed. 0 for no aperiodicity.
         ap_pulse_delay = 11;                        % ms, the amount the spike should be delayed. 0 for no aperiodicity.
-%         ap_pulse_num = 0;  % ms, the amount the spike should be delayed. 0 for no aperiodicity.
+        ap_pulse_num = 0;  % ms, the amount the spike should be delayed. 0 for no aperiodicity.
         pulse_train_preset = 1;     % Preset number to use for manipulation on pulse train (see getDeltaTrainPresets.m for details; 0-no manipulation; 1-aperiodic pulse; etc.)
         PPtauRx = tauAMPAr+jitter_rise;      % Broaden by fixed amount due to presynaptic jitter
         kernel_type = 1;
--- save_allfigs_Dave.m
+++ save_allfigs_Dave.m
@@ -15,10 +15,10 @@ function save_allfigs_Dave(handles_arr)
     
     do_commit = 1;
     supersize_me = 0;
-    handles_arr = [3:4];
+    handles_arr = [1:4];
     if isempty(handles_arr); handles_arr = 1:2; end
     currfname = 'kr'; 
-    currfigname = '161b_add_deepFS_AP';
+    currfigname = '161c_add_deepFS_noAP';
     savenames={'fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8','fig9','fig10','fig11','fig12','fig13','fig14','fig15','fig16','fig17','fig18','fig19','fig20','fig21','fig22','fig23','fig24','fig25','fig26','fig27','fig28','fig29','fig30'};
     mydate = datestr(datenum(date),'yy/mm/dd'); mydate = strrep(mydate,'/','');
     c=clock;
@@ -87,7 +87,7 @@ function save_allfigs_Dave(handles_arr)
         end
     end
     %
-    mycomment = ['As before. Driven with AP.'];
+    mycomment = ['As before. Driven with no AP. Figs [1,2] and [3,4] are exact same sim parameters, but different random seed!'];
     
     % Write to a text file
     fileID = fopen(fullfile(basepath,sp,'readme.txt'),'w');
