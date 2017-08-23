95d6cd59aaf824b2688e8468e1761431aefbc6ef
David Stanley <davearthur.stanley@gmail.com>
2017-08-22 22:22:18 -0400

    Added some figure scripts to kramer_IB_deltapaper_scripts.m

--- kramer_IB/kramer_IB_deltapaper_scripts.m
+++ kramer_IB/kramer_IB_deltapaper_scripts.m
@@ -3,21 +3,43 @@
 % will reproduce all the figures in the paper. It works by calling
 % kramer_IB_function_mode, which in turn runs kramer_IB.m
 
-%% Setup
-f = 0;
+%% Figures 1 Test turning on/off PPStim for various populations
+% Setup
 clear s
+f = 0;
 
-%% Figures
+% Simulation batch 1
 f = f + 1;
 s{f} = struct;
 s{f}.parallel_flag = 1;
 i=0;
-i=i+1; s{f}.vary{i} = {'(IB,RS)','PP_gSYN',[0.25, 0.2]};     % Rows are applied to populations
-i=i+1; s{f}.vary{i} = {'(IB,RS)','PP_gSYN',[0.00, 0.2]};
-i=i+1; s{f}.vary{i} = {'(IB,RS)','PP_gSYN',[0.25, 0.0]};
+i=i+1; s{f}.vary{i} = {'(IB,RS,FS)','PP_gSYN',[0.25, 0.2, 0.2]};     % Rows are applied to populations
+i=i+1; s{f}.vary{i} = {'(IB,RS,FS)','PP_gSYN',[0.00, 0.2, 0.1]};
+i=i+1; s{f}.vary{i} = {'(IB,RS,FS)','PP_gSYN',[0.25, 0.0, 0.0]};
 
 clear data;
 for f = 1:length(s)
-    data{f} = kramer_IB_function_mode(s{f});
+    datac{f} = kramer_IB_function_mode(s{f});
 end
+data = datac{1};
 
+
+
+%% Figures 2 Test varying stim frequency
+
+% Setup
+clear s
+f = 0;
+
+% Simulation batch 1
+f = f + 1;
+s{f} = struct;
+s{f}.parallel_flag = 1;
+i=0;
+i=i+1; s{f}.vary = {'(IB,RS,FS)','PPfreq',[10,30,50,70]};     % Rows are applied to populations
+
+clear data;
+for f = 1:length(s)
+    datac{f} = kramer_IB_function_mode(s{f});
+end
+data = datac{1};
