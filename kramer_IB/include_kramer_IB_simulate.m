47c7b60b66fd60c6047a654e0a85293540363d9a
David Stanley <davearthur.stanley@gmail.com>
2017-08-23 18:56:25 -0400

    Updated iNMDA.mech to record synaptic currents.

--- kramer_IB/include_kramer_IB_simulate.m
+++ kramer_IB/include_kramer_IB_simulate.m
@@ -89,7 +89,8 @@ if include_IB && include_NG && include_FS && include_dFS5; data = dsThevEquiv(da
 if include_IB && include_NG && include_FS && ~include_dFS5; data = dsThevEquiv(data,{'IB_NG_IBaIBdbiSYNseed_ISYN','IB_NG_iGABABAustin_IGABAB','IB_FS_IBaIBdbiSYNseed_ISYN'},'IB_V',[-95,-95,-95],'IB_THALL_GABA'); end
 if include_IB && include_FS && include_dFS5; data = dsThevEquiv(data,{'IB_dFS5_IBaIBdbiSYNseed_ISYN'},'IB_V',[-95],'IB_FS_GABAA'); end  % GABA A only
 if include_IB && include_FS && ~include_dFS5; data = dsThevEquiv(data,{'IB_FS_IBaIBdbiSYNseed_ISYN'},'IB_V',[-95],'IB_FS_GABAA'); end  % GABA A only
-if include_IB && include_NG; data = dsThevEquiv(data,{'IB_NG_IBaIBdbiSYNseed_ISYN','IB_NG_iGABABAustin_IGABAB'},'IB_V',[-95,-95],'IB_NG_GABAB'); end
+if include_IB && include_NG; data = dsThevEquiv(data,{'IB_NG_IBaIBdbiSYNseed_ISYN','IB_NG_iGABABAustin_IGABAB'},'IB_V',[-95,-95],'IB_NG_GABAall'); end
+if include_IB; data = dsThevEquiv(data,{'IB_IB_IBaIBdbiSYNseed_ISYN','IB_IB_iNMDA_ISYN'},'IB_V',[0,0],'IB_IB_AMPANMDA'); end
 
 % % Calculate averages across cells (e.g. mean field)
 data2 = dsCalcAverages(data);
