12bd5857a0eeae37d10cbcda22046a09ecbe962e
benpolletta <benpolletta@gmail.com>
2017-10-31 16:41:27 -0400

    PRC_plot: changing timescale and time limits on plots.

--- kramer_IB/funcs_Ben/PRC_plot.m
+++ kramer_IB/funcs_Ben/PRC_plot.m
@@ -1,4 +1,10 @@
-function PRC_plot(data, results, name)
+function results = PRC_plot(data, results, name)
+
+if isempty(results)
+    
+   results = dsAnalyze(data, @PRC_metrics);
+    
+end
 
 stim = [data.deepRS_STPstim]';
 stims = unique(stim);
@@ -13,9 +19,9 @@ following_spike_times = [results.following_spike_times]';
 cycle_lengths = following_spike_times - repmat([results.triggering_spike_time]', 1, 2);
 baseline_cycle_lengths = diag(nanmean(cycle_lengths(stim == 0, :)));
 baseline_cycle_mat = ones(size(cycle_lengths))*baseline_cycle_lengths;
-phase_advances = -(cycle_lengths - baseline_cycle_mat)./baseline_cycle_mat;
+phase_advances = -(cycle_lengths - baseline_cycle_mat); % ./baseline_cycle_mat;
 
-phase_shifts = repmat(shifts, 1, 2)*diag(1./diag(baseline_cycle_lengths));
+phase_shifts = repmat(shifts, 1, 2); % *diag(1./diag(baseline_cycle_lengths));
 
 phase_advances = reshape(phase_advances, length(stims), length(phase_shifts), 2);
 
@@ -25,7 +31,7 @@ phase_advances = reshape(phase_advances, length(stims), length(phase_shifts), 2)
 
 figure
 
-subplot(211)
+subplot(121)
 
 plot(phase_shifts(:, 1), phase_advances(:, :, 1)) % boundedline(phase_shifts, mean_pa, prep_for_boundedline(std_pa)) % 
 
@@ -33,22 +39,30 @@ for s = 1:length(stims), stim_labels{s} = sprintf('STPstim = %g', abs(stims(s)))
 legend(fliplr(stim_labels))
 
 axis tight
+box off
+xlim([0 baseline_cycle_lengths(1)])
 set(gca, 'FontSize', 16)
-xlabel('Phase of Input Pulse (2\pi)')
-ylabel('Spike Phase Change (2\pi)')
+xlabel('Input Pulse Delay (ms)') % 'Phase of Input Pulse (2\pi)')
+ylabel('Spike Time Advance (ms)') % 'Spike Phase Change (2\pi)')
 title(sprintf('Phase Response Curves, First Spike, Period = %g', baseline_cycle_lengths(1,1)))
 
-subplot(212)
+subplot(122)
 
 plot(phase_shifts(:, 2), phase_advances(:, :, 2)) % boundedline(phase_shifts, mean_pa, prep_for_boundedline(std_pa)) % 
 
-for s = 1:length(stims), stim_labels{s} = sprintf('STPstim = %g', abs(stims(s))); end
-legend(fliplr(stim_labels))
+hold on
+
+plot(repmat(baseline_cycle_lengths(1), 2, 1), ylim', 'k:', 'LineWidth', 2)
+
+my_legend = fliplr(stim_labels);
+my_legend{end + 1} = 'Time of 1st Spike';
+legend(my_legend)
 
 axis tight
+box off
 set(gca, 'FontSize', 16)
-xlabel('Phase of Input Pulse (2\pi)')
-ylabel('Spike Phase Change (2\pi)')
+xlabel('Input Pulse Delay (ms)') % 'Phase of Input Pulse (2\pi)')
+ylabel('Spike Time Advance (ms)') % 'Spike Phase Change (2\pi)')
 title(sprintf('Phase Response Curves, Second Spike, Period = %g', baseline_cycle_lengths(2,2)))
 
 save_as_pdf(gcf, [name, '_PRC'])
