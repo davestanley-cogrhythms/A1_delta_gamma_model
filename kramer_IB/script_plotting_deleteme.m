6e16cd7147b47800d2384dba0b2543d6e18f519d
David Stanley <davearthur.stanley@gmail.com>
2017-12-01 18:40:00 -0500

    Started writing power spectrum code

--- kramer_IB/script_plotting_deleteme.m
+++ kramer_IB/script_plotting_deleteme.m
@@ -36,3 +36,16 @@ gGABAb_ngng*Nng
 
 gGABAa_ngib*Nng
 gGABAb_ngib*Nng
+
+
+%% Plot LFPs
+dsPlot2_PPStim(data,'population','IB','variable','/AMPANMDA_gTH|THALL_GABA_gTH|GABAall_gTH/','do_mean',true,'xlims',ind_range,'ylims',[0 0.5],'force_last','variable','LineWidth',2)
+
+dsPlot2(data,'plot_type','power','xlims',[0 80],'population','RS','variable','IB_IBaIBdbiSYNseed_ISYN','do_mean',1)
+
+dsPlot2(data,'plot_type','power','xlims',[0 80],'population','RS','variable','/LFPall_gTH|LFPdelta_gTH|LFPgamma_gTH/','do_mean',1,'LineWidth',2);
+
+dsPlot2(data,'plot_type','waveform','population','RS','variable','/LFPall_gTH|LFPdelta_gTH|LFPgamma_gTH/','do_mean',1,'LineWidth',2);
+
+
+
