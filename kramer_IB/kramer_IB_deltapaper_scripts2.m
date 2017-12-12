529b502c83b78f928273960b97320a43407e0a83
David Stanley <davearthur.stanley@gmail.com>
2017-12-12 18:01:50 -0500

    Added case 0a for compiling

--- kramer_IB/kramer_IB_deltapaper_scripts2.m
+++ kramer_IB/kramer_IB_deltapaper_scripts2.m
@@ -17,7 +17,86 @@ namesuffix = '_gar0.25';
 
 switch chosen_cell
     case '0a'
+        %% Compile - compile for all cells
+        clear s
+        f = 1;
+        s{f} = struct;
+        s{f}.save_figures = 0;
+        s{f}.sim_mode = 1;
+        s{f}.pulse_mode = 1;
         
+        % % % % % Cells to include in model
+        s{f}.include_IB =   1;
+        s{f}.include_RS =   1;
+        s{f}.include_FS =   1;
+        s{f}.include_LTS =  1;
+        s{f}.include_NG =   1;
+        s{f}.include_dFS5 = 1;
+        s{f}.include_deepRS = 0;
+        s{f}.include_deepFS = 0;
+        
+        datapf0a = kramer_IB_function_mode(s{f},f);
+        
+        
+        %% Compile - compile for just delta oscillator + FS resetter
+        clear s
+        f = 1;
+        s{f} = struct;
+        s{f}.save_figures = 0;
+        s{f}.sim_mode = 1;
+        s{f}.pulse_mode = 1;
+        
+        % % % % % Cells to include in model
+        s{f}.include_IB =   1;
+        s{f}.include_RS =   0;
+        s{f}.include_FS =   0;
+        s{f}.include_LTS =  0;
+        s{f}.include_NG =   1;
+        s{f}.include_dFS5 = 1;
+        s{f}.include_deepRS = 0;
+        s{f}.include_deepFS = 0;
+        
+        datapf0a = kramer_IB_function_mode(s{f},f);
+        
+        %% Compile - Small jobs - Just delta oscillator
+        clear s
+        f = 1;
+        s{f} = struct;
+        s{f}.save_figures = 0;
+        s{f}.sim_mode = 1;
+        s{f}.pulse_mode = 1;
+        
+        % % % % % Cells to include in model
+        s{f}.include_IB =   1;
+        s{f}.include_RS =   0;
+        s{f}.include_FS =   0;
+        s{f}.include_LTS =  0;
+        s{f}.include_NG =   1;
+        s{f}.include_dFS5 = 0;
+        s{f}.include_deepRS = 0;
+        s{f}.include_deepFS = 0;
+        
+        datapf0a = kramer_IB_function_mode(s{f},f);
+        
+        %% Compile - Small jobs - Just IB cells
+        clear s
+        f = 1;
+        s{f} = struct;
+        s{f}.save_figures = 0;
+        s{f}.sim_mode = 1;
+        s{f}.pulse_mode = 1;
+        
+        % % % % % Cells to include in model
+        s{f}.include_IB =   1;
+        s{f}.include_RS =   0;
+        s{f}.include_FS =   0;
+        s{f}.include_LTS =  0;
+        s{f}.include_NG =   0;
+        s{f}.include_dFS5 = 0;
+        s{f}.include_deepRS = 0;
+        s{f}.include_deepFS = 0;
+        
+        datapf0a = kramer_IB_function_mode(s{f},f);        
         
         
     case '1a'
