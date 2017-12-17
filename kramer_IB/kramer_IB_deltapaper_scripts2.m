3c1da8181358f8efd486eac2d214247e937a4824
David Stanley <davearthur.stanley@gmail.com>
2017-12-17 00:27:55 -0500

    Fixed small error in previous commit pertaining to figheight

--- kramer_IB/kramer_IB_deltapaper_scripts2.m
+++ kramer_IB/kramer_IB_deltapaper_scripts2.m
@@ -484,9 +484,12 @@ switch chosen_cell
         
         % Add a plot
         plot_options.linecolor = 'k';
+        chosen_height = 1/3;
         s{f}.plot_func = @dsPlot2;
-        s{f}.parallel_plot_entries_additional{1} = {'population','IB','variable','/V/','do_mean',true,'force_last','varied1','LineWidth',2,'plot_type','waveformErr','lock_axes',false,'Ndims_per_subplot',3,'plot_options',plot_options};
-        s{f}.parallel_plot_entries_additional{2} = {'population','IB','variable','/THALL_GABA_gTH|GABAall_gTH|AMPANMDA_gTH/','do_mean',true,'force_last','varied1','LineWidth',2,'plot_type','waveformErr','lock_axes',false,'Ndims_per_subplot',3};
+        s{f}.parallel_plot_entries_additional{1} = {'population','IB','variable','/V/','do_mean',true,'force_last','varied1','LineWidth',2,'plot_type','waveformErr','lock_axes',false,'Ndims_per_subplot',3,'plot_options',plot_options,...
+            'figheight',chosen_height};
+        s{f}.parallel_plot_entries_additional{2} = {'population','IB','variable','/THALL_GABA_gTH|GABAall_gTH|AMPANMDA_gTH/','do_mean',true,'force_last','varied1','LineWidth',2,'plot_type','waveformErr','lock_axes',false,'Ndims_per_subplot',3,...
+            'figheight',chosen_height};
         
         % Adjust AR
         % s{f}.gAR_d=0.5;
