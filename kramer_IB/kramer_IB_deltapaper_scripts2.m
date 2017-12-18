be60c3440a068ef4488e07af0bdad0acf4ecc5b6
David Stanley <davearthur.stanley@gmail.com>
2017-12-17 22:35:52 -0500

    Condensed blk_M and blk_H options into Fig 9a for kramer_IB_deltapaper_scripts2

--- kramer_IB/kramer_IB_deltapaper_scripts2.m
+++ kramer_IB/kramer_IB_deltapaper_scripts2.m
@@ -439,9 +439,22 @@ switch chosen_cell
             % plots together.
         % Setup
         short_mode = false;  % If true, do a shorter sim
+        blk_h_current = false;        
+        blk_m_current = false;
         clear s
         f=1;
         s{f} = struct;
+        
+        if blk_h_current
+            namesuffix = [namesuffix '_blkgAR'];
+            s{f}.gAR_d = 0;
+        end
+        
+        if blk_m_current
+            namesuffix = [namesuffix '_blkgM'];
+            s{f}.gM_d = 0.5;        % Don't fully block, just reduce it substantially
+        end
+        
         s{f}.save_figures_move_to_Figs_repo = true; s{f}.save_figures = 1;
         s{f}.repo_studyname = ['DeltaFig9a_polley'  num2str(f) '' namesuffix];
         s{f}.sim_mode = 1;
@@ -524,101 +537,7 @@ switch chosen_cell
 
         datapf9b = kramer_IB_function_mode(s{f},f);
         
-    case '9c'
-        %% Paper 9c - As 9a but block m current
-        % Setup
-        clear s
-        f=1;
-        s{f} = struct;
-        s{f}.save_figures_move_to_Figs_repo = true; s{f}.save_figures = 1;
-        s{f}.repo_studyname = ['DeltaFig9c_mblk'  num2str(f) '' namesuffix];
-        s{f}.sim_mode = 1;
-        s{f}.pulse_mode = 6;
-        
-        
-        s{f}.PPmaskfreq = 1;    % Do a pulse every 1 second
-        s{f}.PPonset = 3950;    % Just let the pulse at 4000 through
-        s{f}.PPoffset = 4500;
-        
-        % Shuffle through a bunch of values
-        s{f}.vary = { ...
-            'RS','myshuffle',1:8;...
-            };
-        s{f}.random_seed = 'shuffle';
-        s{f}.parallel_flag = 1; s{f}.maxNcores = maxNcores;
-        s{f}.pulse_mode = 6;
-        s{f}.tspan=[0 5500];
-        
-        s{f}.gM_d = 0.5;        % Don't fully block, just reduce it substantially
 
-        datapf9c = kramer_IB_function_mode(s{f},f);
-    case '9d'
-        %% Paper 9d - As 9a but block h current
-        % Setup
-        short_mode = false;  % If true, do a shorter sim
-        clear s
-        f=1;
-        s{f} = struct;
-        s{f}.save_figures_move_to_Figs_repo = true; s{f}.save_figures = 1;
-        s{f}.repo_studyname = ['DeltaFig9d_polley'  num2str(f) '' namesuffix];
-        s{f}.sim_mode = 1;
-        s{f}.pulse_mode = 6;
-        
-        
-        s{f}.PPmaskfreq = 1;    % Do a pulse every 1 second
-        s{f}.PPonset = 3950;    % Just let the pulse at 4000 through
-        s{f}.PPoffset = 4500;
-        
-%         % Shuffle through a bunch of values        
-        s{f}.vary = { ...
-            'RS','myshuffle',1:16;...
-            };
-%         s{f}.vary = { ...
-%             'IB','stim2',[0:0.5:1.5, 2:5];...
-%             };
-
-        % Reduce Ncells
-%         s{f}.Nrs = 20;
-
-        s{f}.random_seed = 'shuffle';
-        s{f}.parallel_flag = 1; s{f}.maxNcores = maxNcores;
-        s{f}.pulse_mode = 6;
-        s{f}.tspan=[0 5500];
-        
-        % Shorten some sim parameters for short mode
-        if short_mode
-          s{f}.tspan=[0 2500];
-          s{f}.PPonset = 950;
-          s{f}.PPoffset = 1500;
-          s{f}.vary = {'RS','myshuffle',1:8};
-        end
-        
-        % Modifications to each sim have random IC's and independent noise
-        s{f}.Jng1=1;   % NG cells
-        %s{f}.IC_noise = 0.5;
-        s{f}.syn_ngib_IC_noise = 0.5;
-        s{f}.random_seed = 'shuffle';
-        
-        % Add a plot
-        plot_options.linecolor = 'k';
-        chosen_height = 1/3;
-        s{f}.plot_func = @dsPlot2;
-        % Full range
-        s{f}.parallel_plot_entries_additional{1} = {'population','IB','variable','/V/','do_mean',true,'force_last','varied1','LineWidth',2,'plot_type','waveformErr','lock_axes',false,'Ndims_per_subplot',3,'plot_options',plot_options,...
-            'figheight',chosen_height};
-        s{f}.parallel_plot_entries_additional{2} = {'population','IB','variable','/THALL_GABA_gTH|GABAall_gTH|AMPANMDA_gTH/','do_mean',true,'force_last','varied1','LineWidth',2,'plot_type','waveformErr','lock_axes',false,'Ndims_per_subplot',3,...
-            'figheight',chosen_height};
-        
-        % 1000 ms
-        s{f}.parallel_plot_entries_additional{3} = {'population','IB','variable','/V/','do_mean',true,'force_last','varied1','LineWidth',2,'plot_type','waveformErr','lock_axes',false,'Ndims_per_subplot',3,'plot_options',plot_options,...
-            'xlims',[4000,5500],'figheight',chosen_height};
-        s{f}.parallel_plot_entries_additional{4} = {'population','IB','variable','/THALL_GABA_gTH|GABAall_gTH|AMPANMDA_gTH/','do_mean',true,'force_last','varied1','LineWidth',2,'plot_type','waveformErr','lock_axes',false,'Ndims_per_subplot',3,...
-            'xlims',[4000,5500],'figheight',chosen_height};
-        
-        % Adjust AR
-        s{f}.gAR_d = 0;
-
-        datapf9d = kramer_IB_function_mode(s{f},f);
 end
 
 end
