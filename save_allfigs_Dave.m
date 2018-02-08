505e90e3ef81cb843cda47e489a53dd27a4075bc
David Stanley <davearthur.stanley@gmail.com>
2018-02-07 22:43:45 -0500

    Added additional files to be included in .zip

--- save_allfigs_Dave.m
+++ save_allfigs_Dave.m
@@ -66,7 +66,8 @@ function save_allfigs_Dave(study_dir,spec_all,handles_arr,do_commit,currfigname)
     end
     
     % Save .m file
-    zip(fullfile(basepath,sp,'kramer_IB.zip'),{'kramer_IB.m','include_kramer_IB_populations.m','include_kramer_IB_synapses.m'});
+    zip(fullfile(basepath,sp,'kramer_IB.zip'),{'kramer_IB.m','include_kramer_IB_populations.m','include_kramer_IB_synapses.m','include_kramer_IB_simulate.m','include_kramer_IB_plotting.m', ...
+        'kramer_IB_deltapaper_scripts2.m','kramer_IB_deltapaper_scripts1.m','kramer_IB_deltapaper_tune1.m','kramer_IB_clustersub.m '});
     
     if ~isempty(study_dir)
         % Copy study info file
