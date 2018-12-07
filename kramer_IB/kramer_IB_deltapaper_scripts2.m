931339a789c6505a33202815f812bbbcaeb48306
David Stanley <davearthur.stanley@gmail.com>
2018-12-07 16:49:51 -0500

    Added option for just compiling the superficial oscillator

--- kramer_IB/kramer_IB_deltapaper_scripts2.m
+++ kramer_IB/kramer_IB_deltapaper_scripts2.m
@@ -100,6 +100,26 @@ switch chosen_cell
         
         datapf0a = kramer_IB_function_mode(s{f},f);        
         
+        %% Compile - Superficial layer only
+        clear s
+        f = 1;
+        s{f} = struct;
+        s{f}.save_figures = 0;
+        s{f}.sim_mode = 1;
+        s{f}.pulse_mode = 1;
+        
+        % % % % % Cells to include in model
+        s{f}.include_IB =   0;
+        s{f}.include_RS =   1;
+        s{f}.include_FS =   1;
+        s{f}.include_LTS =  1;
+        s{f}.include_NG =   0;
+        s{f}.include_dFS5 = 0;
+        s{f}.include_deepRS = 0;
+        s{f}.include_deepFS = 0;
+        
+        datapf0a = kramer_IB_function_mode(s{f},f);    
+        
         
     case '1a1'
         %% Paper Figs 1a - Pulse train no AP
