f4184cee0aed69c12e50852f77cdbfd20f087ec7
David Stanley <davearthur.stanley@gmail.com>
2018-12-09 18:16:07 -0500

    Renamed namesuffix to _PPfreqSweep_v1.3 to match current tag of model

--- kramer_IB/kramer_IB_deltapaper_scripts2.m
+++ kramer_IB/kramer_IB_deltapaper_scripts2.m
@@ -12,7 +12,7 @@ if nargin < 2
     maxNcores = Inf;
 end
 
-namesuffix = '_hcurrent6';
+namesuffix = '_PPfreqSweep_v1.3';
 % namesuffix = '_IBPPStim0.05';
 % namesuffix = '_gar0.0';
 % namesuffix = '';
