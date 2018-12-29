f7270ba3f4bd0c728d9f9435a0b0a4ec19b7e305
David Stanley <davearthur.stanley@gmail.com>
2018-12-29 16:11:42 -0500

    startup_editor.m - Added command to automatically open scripts in editor

---
+++ startup_editor.m
@@ -0,0 +1,11 @@
+
+%% Open default set of scripts for editing
+edit kramer_IB/kramer_IB_deltapaper_tune1
+edit kramer_IB/kramer_IB_deltapaper_scripts2
+edit kramer_IB/kramer_IB_clustersub
+edit kramer_IB/kramer_IB
+edit kramer_IB/include_kramer_IB_populations
+edit kramer_IB/include_kramer_IB_synapses
+edit kramer_IB/include_kramer_IB_simulate
+edit kramer_IB/include_kramer_IB_plotting
+edit save_allfigs_Dave
