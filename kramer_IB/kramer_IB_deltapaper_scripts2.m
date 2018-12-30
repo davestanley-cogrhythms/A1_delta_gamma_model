0f91d41fb189e20859e4ba08a5cf4acc75ee5037
David Stanley <davearthur.stanley@gmail.com>
2018-12-29 21:08:36 -0500

    Reduced IB PPStim to 0.2

--- kramer_IB/kramer_IB_deltapaper_scripts2.m
+++ kramer_IB/kramer_IB_deltapaper_scripts2.m
@@ -12,7 +12,7 @@ if nargin < 2
     maxNcores = Inf;
 end
 
-namesuffix = '_IBPPStim0.3';
+namesuffix = '_IBPPStim0.2';
 % namesuffix = '_IBPPStim0.05';
 % namesuffix = '_gar0.0';
 % namesuffix = '';
