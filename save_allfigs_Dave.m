39c7ac30886767f4204aaedd8cdf0927316f3307
David Stanley <davearthur.stanley@gmail.com>
2019-01-03 01:36:05 -0500

    195b_redoprev_longsim

--- save_allfigs_Dave.m
+++ save_allfigs_Dave.m
@@ -21,9 +21,9 @@ function [outpath] = save_allfigs_Dave(study_dir,spec_all,handles_arr,do_commit,
     supersize_me = 0;
     
     if strcmp(calledby(0), 'root')      % Commands inside here will only execute when running this code in cell mode (e.g. not as a function)
-        handles_arr = 1:6;
+        handles_arr = 1:4;
         do_commit = 1;
-        currfigname = '195a_sweep_IB_gRAN';
+        currfigname = '195b_redoprev_longsim';
         if ~exist('study_dir','var'); study_dir = []; end
     end
     
@@ -110,7 +110,7 @@ function [outpath] = save_allfigs_Dave(study_dir,spec_all,handles_arr,do_commit,
 %         end
 %     end
 %     %
-    mycomment = ['Spontaneous activity. Goal: Working on low GABAB oscillator, so it will be more easy to reset. First, just decreased excitatory drive to IB cells to see how the oscillator behaves at low drive.'];
+    mycomment = [''];
     
     % Write to a text file
     fileID = fopen(fullfile(basepath,sp,'readme.txt'),'w');
