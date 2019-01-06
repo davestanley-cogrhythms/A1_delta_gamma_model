80bd404a9b8bd107413499a800a39563d6e8c706
David Stanley <davearthur.stanley@gmail.com>
2019-01-06 04:58:27 -0500

    197j_sweep2D_NGIB_IBPPstim

--- save_allfigs_Dave.m
+++ save_allfigs_Dave.m
@@ -23,7 +23,7 @@ function [outpath] = save_allfigs_Dave(study_dir,spec_all,handles_arr,do_commit,
     if strcmp(calledby(0), 'root')      % Commands inside here will only execute when running this code in cell mode (e.g. not as a function)
         handles_arr = 1:5;
         do_commit = 1;
-        currfigname = '197i_sweep2D_NGIB_gabab_IBstim';
+        currfigname = '197j_sweep2D_NGIB_IBPPstim';
         if ~exist('study_dir','var'); study_dir = []; end
     end
     
