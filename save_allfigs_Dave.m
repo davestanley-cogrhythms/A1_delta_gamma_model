3e6316d1e6d057ddbd6f3b8c6a65adf5ffa5bf51
David Stanley <davearthur.stanley@gmail.com>
2019-01-05 19:13:14 -0500

    197g_sweep_PPStim Fixed PPOnset at 800, try different PPStim values

--- save_allfigs_Dave.m
+++ save_allfigs_Dave.m
@@ -23,7 +23,7 @@ function [outpath] = save_allfigs_Dave(study_dir,spec_all,handles_arr,do_commit,
     if strcmp(calledby(0), 'root')      % Commands inside here will only execute when running this code in cell mode (e.g. not as a function)
         handles_arr = 1:6;
         do_commit = 1;
-        currfigname = '197f_inc_IB_PPstim_0.2';
+        currfigname = '197g_sweep_PPStim';
         if ~exist('study_dir','var'); study_dir = []; end
     end
     
@@ -110,7 +110,7 @@ function [outpath] = save_allfigs_Dave(study_dir,spec_all,handles_arr,do_commit,
 %         end
 %     end
 %     %
-    mycomment = ['Was previously 0.1, and 0.4 before that!'];
+    mycomment = ['Fixed PPOnset at 800, try different PPStim values'];
     
     % Write to a text file
     fileID = fopen(fullfile(basepath,sp,'readme.txt'),'w');
