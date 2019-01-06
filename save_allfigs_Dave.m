556c2d6b029c08b46443974a0b948aa263356574
David Stanley <davearthur.stanley@gmail.com>
2019-01-06 15:17:02 -0500

    197o_redoPrev_sweep2D_jIB_0.0

--- save_allfigs_Dave.m
+++ save_allfigs_Dave.m
@@ -21,9 +21,9 @@ function [outpath] = save_allfigs_Dave(study_dir,spec_all,handles_arr,do_commit,
     supersize_me = 0;
     
     if strcmp(calledby(0), 'root')      % Commands inside here will only execute when running this code in cell mode (e.g. not as a function)
-        handles_arr = 1:7;
+        handles_arr = 1:6;
         do_commit = 1;
-        currfigname = '197n_redoPrev_sweep2D_jIB_0.25';
+        currfigname = '197o_redoPrev_sweep2D_jIB_0.0';
         if ~exist('study_dir','var'); study_dir = []; end
     end
     
