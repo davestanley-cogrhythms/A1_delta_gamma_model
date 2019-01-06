10e9d56098f98e66d1af8262e85b74ddd4400ab9
David Stanley <davearthur.stanley@gmail.com>
2019-01-06 05:10:40 -0500

    197k_redoPrev_diffRange

--- save_allfigs_Dave.m
+++ save_allfigs_Dave.m
@@ -21,9 +21,9 @@ function [outpath] = save_allfigs_Dave(study_dir,spec_all,handles_arr,do_commit,
     supersize_me = 0;
     
     if strcmp(calledby(0), 'root')      % Commands inside here will only execute when running this code in cell mode (e.g. not as a function)
-        handles_arr = 1:5;
+        handles_arr = 1:6;
         do_commit = 1;
-        currfigname = '197j_sweep2D_NGIB_IBPPstim';
+        currfigname = '197k_redoPrev_diffRange';
         if ~exist('study_dir','var'); study_dir = []; end
     end
     
