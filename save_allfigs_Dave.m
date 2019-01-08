a8c7defe195b9e964a2f6a5739ff7743226006c6
David Stanley <davearthur.stanley@gmail.com>
2019-01-08 17:56:14 -0500

    198k_reduced_simMode20

--- save_allfigs_Dave.m
+++ save_allfigs_Dave.m
@@ -21,9 +21,9 @@ function [outpath] = save_allfigs_Dave(study_dir,spec_all,handles_arr,do_commit,
     supersize_me = 0;
     
     if strcmp(calledby(0), 'root')      % Commands inside here will only execute when running this code in cell mode (e.g. not as a function)
-        handles_arr = 1:25;
+        handles_arr = 1:11;
         do_commit = 1;
-        currfigname = '198i_sweep_simMode21';
+        currfigname = '198k_reduced_simMode20';
         if ~exist('study_dir','var'); study_dir = []; end
     end
     
