1d49854a546bc58cd0b4ecb235f8bada7e1131d2
David Stanley <davearthur.stanley@gmail.com>
2019-01-10 18:40:40 -0500

    d190110_t184039_201f_dec_gRAN_sm20pm1 Reduce gRAN. Goal: See if reducing noise can reduce delay caused by IB partial bursts.

--- kramer_IB/kramer_IB.m
+++ kramer_IB/kramer_IB.m
@@ -42,8 +42,8 @@ save_figures = 0;               % Save figures associated with individusl sims, 
     save_composite_figures = 0;     % Flag for saving composite figures comprised of PNGs stitched together from individual simulations into a single large canvas
 
 % % % % % Display options
-save_combined_figures = 1;      % Flag for figures based on post-hoc analysis of all sims together
-plot_on = 0;
+save_combined_figures = 0;      % Flag for figures based on post-hoc analysis of all sims together
+plot_on = 1;
 plot_on2 = 0;
 do_visible = 'off';
 
