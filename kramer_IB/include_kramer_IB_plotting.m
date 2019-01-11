9d14f564470512c06d81abb1721a2486a52d710b
David Stanley <davearthur.stanley@gmail.com>
2019-01-11 12:22:29 -0500

    Plotting - commented out some plotting and grouped case 16 with case 9 and 12

--- kramer_IB/include_kramer_IB_plotting.m
+++ kramer_IB/include_kramer_IB_plotting.m
@@ -412,26 +412,33 @@ if plot_on
             
         case {5,6}
 
-        case {8,9,10,12}
+        case {8,9,10,12,16}
             %% 2D plots
             inds = 1:length(data);
+            
+            do_transpose = false;           % Transposes rows and columns of subplots
+            xp = dsAll2mdd(data(inds));
+            
+            if do_transpose
+                % Optional correction - flip x/y varied; not guranteed to work in all
+                % situations
+                xp = dsAll2mdd(data); xp=xp.permute([2,1,3,4]);
+            end
 
             NESP = 1;
             plot2_figure_options = {'visible',do_visible,'num_embedded_subplots',NESP,'xlims',ind_range};
-            dsPlot2_PPStim(data,'population','all','do_mean',1,'ylims',[-95,-40],plot2_figure_options{:})
-            %dsPlot2_PPStim(data,'population','all','do_mean',1,'ylims',[-95,-65],plot2_figure_options{:})
-            dsPlot2_PPStim(data,'plot_type','raster','population','IB',plot2_figure_options{:});
-            dsPlot2_PPStim(data,'plot_type','raster','population','NG',plot2_figure_options{:});
-            dsPlot2_PPStim(data,'population','IB','xlims',ind_range,'plot_type','waveform','max_num_overlaid',1,plot2_figure_options{:});
-    %         dsPlot2_PPStim(data,'population','IB','variable','/AMPANMDA_gTH|THALL_GABA_gTH|GABAall_gTH|iNMDA_s/','do_mean',true,'xlims',ind_range,'ylims',[0 0.4],'force_last','variable','LineWidth',2,plot2_figure_options{:})
-            dsPlot2_PPStim(data,'population','IB','variable','/THALL_GABA_gTH|GABAall_gTH|iNMDA_s/','do_mean',true,'xlims',ind_range,'ylims',[0 0.4],'force_last','variable','LineWidth',2,plot2_figure_options{:})
-            dsPlot2_PPStim(data,'population','/IB/','variable','/iGABABAustin_g/','xlims',ind_range,'do_mean',true,'LineWidth',2,'ylims',[0 3],plot2_figure_options{:});
-            dsPlot2_PPStim(data,'population','/IB/','variable','mAR','xlims',ind_range,'do_mean',true,'LineWidth',2,plot2_figure_options{:})
-            dsPlot2_PPStim(data,'population','/IB/','variable','Mich','xlims',ind_range,'do_mean',true,'LineWidth',2,plot2_figure_options{:})
+            %dsPlot2_PPStim(xp,'population','all','do_mean',1,'ylims',[-95,-40],plot2_figure_options{:})
+            %dsPlot2_PPStim(xp,'population','all','do_mean',1,'ylims',[-95,-65],plot2_figure_options{:})
+            dsPlot2_PPStim(xp,'plot_type','raster','population','IB',plot2_figure_options{:});
+            %dsPlot2_PPStim(xp,'plot_type','raster','population','NG',plot2_figure_options{:});
+            %dsPlot2_PPStim(xp,'population','IB','xlims',ind_range,'plot_type','waveform','max_num_overlaid',1,plot2_figure_options{:});
+    %         dsPlot2_PPStim(xp,'population','IB','variable','/AMPANMDA_gTH|THALL_GABA_gTH|GABAall_gTH|iNMDA_s/','do_mean',true,'xlims',ind_range,'ylims',[0 0.4],'force_last','variable','LineWidth',2,plot2_figure_options{:})
+            dsPlot2_PPStim(xp,'population','IB','variable','/THALL_GABA_gTH|GABAall_gTH|iNMDA_s/','do_mean',true,'xlims',ind_range,'ylims',[0 0.4],'force_last','variable','LineWidth',2,plot2_figure_options{:})
+%             dsPlot2_PPStim(xp,'population','/IB/','variable','/iGABABAustin_g/','xlims',ind_range,'do_mean',true,'LineWidth',2,'ylims',[0 3],plot2_figure_options{:});
+%             dsPlot2_PPStim(xp,'population','/IB/','variable','mAR','xlims',ind_range,'do_mean',true,'LineWidth',2,plot2_figure_options{:})
+%             dsPlot2_PPStim(xp,'population','/IB/','variable','Mich','xlims',ind_range,'do_mean',true,'LineWidth',2,plot2_figure_options{:})
 
-%             % Optional correction - flip x/y varied; not guranteed to work in all
-%             % situations
-%             xp = dsAll2mdd(data); xp=xp.permute([1,3,2,4,5]); data = dsMdd2ds(xp);
+
 % 
 %             % Plot varied parameters individually
 %             N=length(vary{3,3});
