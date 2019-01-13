8cb9ba032ccec4f4331be0d004772f395620b769
David Stanley <davearthur.stanley@gmail.com>
2019-01-13 01:16:12 -0500

    d190113_t011612_207d_inc_delta_to_gammaSYN_pm1

--- kramer_IB/kramer_IB.m
+++ kramer_IB/kramer_IB.m
@@ -129,7 +129,7 @@ gAR_d=0.5; % 155, IBda - max conductance of h-channel
 % gAR_d=0; % 155, IBda - max conductance of h-channel
 % repo_studyname = ['batch01a_gar_' num2str(gAR_d)];
 repo_studyname = ['203a_sweepNMDA_gRAN_0.1_jIB_0.5_pm' num2str(pulse_mode) '_gAR' num2str(gAR_d)];
-repo_studyname = ['207c_unblk_all_pm' num2str(pulse_mode)];
+repo_studyname = ['207d_inc_delta_to_gammaSYN_pm' num2str(pulse_mode)];
 mycomment = ['Test rebound for VERY low tension oscillator (gNGIB=0.7,jIB=1.5). Try to see why its failing to burst. gAR is still 0.5 '];
 mycomment = ['Try increasing gNGIB, since we need to do this to get better superficial modulation'];
 % mycomment = ['Redo_prev'];
