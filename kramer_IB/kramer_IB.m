9810ed80c28f69049f299b4b20ae0c9d495e47ae
David Stanley <davearthur.stanley@gmail.com>
2019-01-13 01:10:21 -0500

    d190113_t011020_207c_unblk_all_pm1

--- kramer_IB/kramer_IB.m
+++ kramer_IB/kramer_IB.m
@@ -129,7 +129,7 @@ gAR_d=0.5; % 155, IBda - max conductance of h-channel
 % gAR_d=0; % 155, IBda - max conductance of h-channel
 % repo_studyname = ['batch01a_gar_' num2str(gAR_d)];
 repo_studyname = ['203a_sweepNMDA_gRAN_0.1_jIB_0.5_pm' num2str(pulse_mode) '_gAR' num2str(gAR_d)];
-repo_studyname = ['207b_blk_dFSIB_pm' num2str(pulse_mode)];
+repo_studyname = ['207c_unblk_all_pm' num2str(pulse_mode)];
 mycomment = ['Test rebound for VERY low tension oscillator (gNGIB=0.7,jIB=1.5). Try to see why its failing to burst. gAR is still 0.5 '];
 mycomment = ['Try increasing gNGIB, since we need to do this to get better superficial modulation'];
 % mycomment = ['Redo_prev'];
@@ -568,7 +568,7 @@ if ~no_synapses
     
     % % Gamma -> Delta connections
     gGABAa_fsib=0.3/Nfs;                        % FS -> IB
-%     gGABAa_fs5ib=0.2/Nfs;    
+    gGABAa_fs5ib=0.2/Nfs;    
     gGABAa_tfs5ib=2/Ntfs5;                    % tFS5 -> IB
     gAMPA_rsib=0.1/Nrs;                         % RS -> IB
 %     gAMPA_rsng = 0.3/Nrs;                       % RS -> NG
