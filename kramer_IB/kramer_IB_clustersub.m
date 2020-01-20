c6a9c73f0af6312e655c2a5140cf8b032bc65260
David Stanley <davearthur.stanley@gmail.com>
2020-01-20 13:34:43 -0500

    Added 13a_p2

--- kramer_IB/kramer_IB_clustersub.m
+++ kramer_IB/kramer_IB_clustersub.m
@@ -30,6 +30,7 @@ clustersub('kramer_IB_deltapaper_scripts2','4a2',13,myhours);        % Lakatos 2
 clustersub('kramer_IB_deltapaper_scripts2','4b',8,myhours);         % As Fig 4a, except use gamma 40 Hz stim instead of Poisson
 clustersub('kramer_IB_deltapaper_scripts2','13a',28,myhours);        % 2D sweeps
 clustersub('kramer_IB_deltapaper_scripts2','13a_p1',28,myhours);     % 2D sweeps (break into parts).
+clustersub('kramer_IB_deltapaper_scripts2','13a_p2',28,myhours);     % 2D sweeps (break into parts).
 
 % Inverse phase-amplitude coupling figures
 clustersub('kramer_IB_deltapaper_scripts2','5a',8,myhours);        % Inverse PAC  
