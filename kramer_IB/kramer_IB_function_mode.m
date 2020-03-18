7976f2f638b676583c42da97947f6fdef085cd47
benpolletta <benpolletta@gmail.com>
2020-03-18 15:09:25 -0400

    kramer_IB_function_mode: adding clause for no vary argument.

--- kramer_IB/kramer_IB_function_mode.m
+++ kramer_IB/kramer_IB_function_mode.m
@@ -55,35 +55,70 @@ include_kramer_IB_populations;
 
 include_kramer_IB_synapses;
 
-save(fullfile(savepath, [name, '_sim_spec.mat']), 'sim_spec', 'sim_struct', 'vary', 'name');
-
 solver
 
-if cluster_flag
+if ~exist('vary', 'var')
     
-    data = dsSimulate(sim_spec,'tspan',tspan,'dt',dt,'downsample_factor',dsfact,'solver',solver,'coder',0,...
-        'random_seed',random_seed,'vary',vary,'verbose_flag',verbose_flag,'cluster_flag',cluster_flag,...
-        'debug_flag',debug_flag,'compile_flag',compile_flag,...
-        'analysis_functions',analysis_functions,'analysis_options',analysis_options,...
-        'overwrite_flag',1,'one_solve_file_flag',1,'qsub_mode',qsub_mode,...
-        'save_data_flag',save_data_flag,'study_dir',fullfile(savepath, name));
+    save(fullfile(savepath, [name, '_sim_spec.mat']), 'sim_spec', 'sim_struct', 'name');
     
-    cd (start_dir)
+    if cluster_flag
+        
+        data = dsSimulate(sim_spec,'tspan',tspan,'dt',dt,'downsample_factor',dsfact,'solver',solver,'coder',0,...
+            'random_seed',random_seed,'verbose_flag',verbose_flag,'cluster_flag',cluster_flag,...
+            'debug_flag',debug_flag,'compile_flag',compile_flag,...
+            'analysis_functions',analysis_functions,'analysis_options',analysis_options,...
+            'overwrite_flag',1,'one_solve_file_flag',1,'qsub_mode',qsub_mode,...
+            'save_data_flag',save_data_flag,'study_dir',fullfile(savepath, name));
+        
+        cd (start_dir)
+        
+        return
+        
+    else
+        
+        tic;
+        
+        data = dsSimulate(sim_spec,'tspan',tspan,'dt',dt,'downsample_factor',dsfact,'solver',solver,'coder',0,... % [data, ~, result]
+            'random_seed',random_seed,'verbose_flag',verbose_flag,'parallel_flag',parallel_flag,'num_cores',num_cores,...
+            'debug_flag',debug_flag,'compile_flag',compile_flag,...
+            'analysis_functions',analysis_functions,'analysis_options',analysis_options,...
+            'save_data_flag',save_data_flag,'study_dir',fullfile(savepath, name));
+        
+        toc;
+        
+    end
     
-    return
-
 else
     
-    tic;
-    
-    data = dsSimulate(sim_spec,'tspan',tspan,'dt',dt,'downsample_factor',dsfact,'solver',solver,'coder',0,... % [data, ~, result]
-        'random_seed',random_seed,'vary',vary,'verbose_flag',verbose_flag,'parallel_flag',parallel_flag,'num_cores',num_cores,...
-        'debug_flag',debug_flag,'compile_flag',compile_flag,...
-        'analysis_functions',analysis_functions,'analysis_options',analysis_options,...
-        'save_data_flag',save_data_flag,'study_dir',fullfile(savepath, name));
-    
-    toc;
+    save(fullfile(savepath, [name, '_sim_spec.mat']), 'sim_spec', 'sim_struct', 'vary', 'name');
+
+    if cluster_flag
         
+        data = dsSimulate(sim_spec,'tspan',tspan,'dt',dt,'downsample_factor',dsfact,'solver',solver,'coder',0,...
+            'random_seed',random_seed,'vary',vary,'verbose_flag',verbose_flag,'cluster_flag',cluster_flag,...
+            'debug_flag',debug_flag,'compile_flag',compile_flag,...
+            'analysis_functions',analysis_functions,'analysis_options',analysis_options,...
+            'overwrite_flag',1,'one_solve_file_flag',1,'qsub_mode',qsub_mode,...
+            'save_data_flag',save_data_flag,'study_dir',fullfile(savepath, name));
+        
+        cd (start_dir)
+        
+        return
+        
+    else
+        
+        tic;
+        
+        data = dsSimulate(sim_spec,'tspan',tspan,'dt',dt,'downsample_factor',dsfact,'solver',solver,'coder',0,... % [data, ~, result]
+            'random_seed',random_seed,'vary',vary,'verbose_flag',verbose_flag,'parallel_flag',parallel_flag,'num_cores',num_cores,...
+            'debug_flag',debug_flag,'compile_flag',compile_flag,...
+            'analysis_functions',analysis_functions,'analysis_options',analysis_options,...
+            'save_data_flag',save_data_flag,'study_dir',fullfile(savepath, name));
+        
+        toc;
+        
+    end
+    
 end
 
 close('all')
