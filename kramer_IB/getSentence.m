7eb52c82e475d4bbe0f072ebc36dca74381489af
benpolletta <benpolletta@gmail.com>
2021-03-04 15:51:13 -0500

    getSentence: Adding arguments for low and high frequency limits, normalization.

--- kramer_IB/getSentence.m
+++ kramer_IB/getSentence.m
@@ -1,13 +1,18 @@
-(function sentence = getSentence(time, no_channels, file_index, onset)
+function sentence = getSentence(time, no_channels, file_index, onset, lowfreq, highfreq, norm)
 
 if nargin < 2, no_channels = []; end
 if isempty(no_channels), no_channels = 1; end
 if nargin < 3, file_index = []; end
 if nargin < 4, onset = []; end
 if isempty(onset), onset = 0; end
+if nargin < 5, lowfreq = []; end
+if isempty(lowfreq), lowfreq = 0; end
+if nargin < 5, highfreq = []; end
+if isempty(highfreq), highfreq = inf; end
+if nargin < 6, norm = []; end
+if isempty(norm), norm = 0; end
 
-tsylb_dir = '/projectnb/crc-nak/brpp/Speech_Stimuli/tsylb2-1.1/';
-addpath(genpath(tsylb_dir))
+nsltools_dir = '/projectnb/crc-nak/brpp/Speech_Stimuli/nsltools/';addpath(genpath(nsltools_dir))
 sentence_dir = '/projectnb/crc-nak/brpp/Speech_Stimuli/timit/TIMIT/';
 
 file_list_id = fopen([sentence_dir, 'wavFileList.txt'], 'r');
@@ -35,17 +40,28 @@ wavfile_length = 100*length(this_wavfile)/16;
 loadload, paras(1) = 1;% paras(4) = 0;
 this_auditory = wav2aud(this_wavfile, paras);
 
+CF = 440 * 2 .^ ((-30:97)/24 - 1);
+CF_included = CF(CF >= lowfreq & CF <= highfreq);
+%CF_limits = [min(CF_included), max(CF_included)];
+this_auditory = this_auditory(:, CF >= lowfreq & CF <= highfreq);
+
 [~, aud_cols] = size(this_auditory);
 
-sentence_length = max(length(time), wavfile_length + onset*100);
+sentence_length = ceil(max(length(time), wavfile_length + onset*100));
 
 cochlear = zeros(sentence_length, no_channels);
 
 channels_averaged = floor(aud_cols/no_channels);
 for c = 1:no_channels
+    %this_channel_freqs(c, :) = CF_included([(c - 1)*channels_averaged + 1, c*channels_averaged]);
     this_channel = nanmean(this_auditory(:,((c - 1)*channels_averaged + 1):c*channels_averaged), 2);
     this_channel = interp(interp(this_channel, 5), 10);
-    cochlear(onset*100 + (1:length(this_channel)), c) = conv(this_channel, ones(1,500)/500, 'same');
+    if norm == 1
+        this_channel = this_channel/sum(this_channel);
+    elseif norm == 2
+        this_channel = this_channel/max(this_channel);
+    end
+    cochlear(onset*100 + (1:length(this_channel)), c) = conv(this_channel, ones(1,500)/500, 'same');  
 end
 
 sentence.cochlear = cochlear;
@@ -59,6 +75,8 @@ syl_boundaries = round(100*syl_boundaries/16);
 syllable_indicator = zeros(size(cochlear, 1), 1);
 
 syllable_indicator(onset*100 + syl_boundaries) = 1;
+
+syllable_indicator = conv(syllable_indicator, ones(1,500)/500, 'same');
 
 sentence.syllables = syllable_indicator;
 
